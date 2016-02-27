a1f637a3ab2031c7f86f1ccbf62c487a22899cfb
Aparna Karve <akarve@redhat.com>
2016-02-26 16:04:36 -0800

    Added RBAC spec tests

--- client/app/services/session.service.spec.js
+++ client/app/services/session.service.spec.js
@@ -29,7 +29,7 @@ describe('Session', function() {
       });
     });
 
-    bard.inject('Session', '$window', '$sessionStorage');
+    bard.inject('Session', '$window', '$sessionStorage', '$httpBackend', 'gettextCatalog', '$state');
   });
 
   describe('switchGroup', function() {
@@ -40,6 +40,61 @@ describe('Session', function() {
 
       expect($sessionStorage.miqGroup).to.eq('good');
       expect(reloadOk).to.eq(true);
+    });
+  });
+
+  describe('setRBAC', function($httpBackend, gettextCatalog, $state) {
+    beforeEach(inject(function(_$httpBackend_, _gettextCatalog_, _$state_) {
+      $httpBackend = _$httpBackend_;
+      gettextCatalog = _gettextCatalog_;
+      $state = _$state_;
+    }));
+
+    it('sets RBAC for actions and navigation', function() {
+      var response = {authorization: {product_features: {
+        service_view: {},
+        service_edit: {}
+      }}};
+      gettextCatalog.loadAndSet = function() {};
+      $httpBackend.whenGET('/api?attributes=authorization').respond(response);
+      Session.loadUser();
+      $httpBackend.flush();
+
+      expect($state.navFeatures.dashboard.show).to.eq(true);
+      expect($state.navFeatures.services.show).to.eq(true);
+      expect($state.navFeatures.requests.show).to.eq(false);
+      expect($state.navFeatures.marketplace.show).to.eq(false);
+
+      expect($state.actionFeatures.service_edit.show).to.eq(true);
+      expect($state.actionFeatures.service_delete.show).to.eq(false);
+      expect($state.actionFeatures.service_reconfigure.show).to.eq(false);
+    });
+
+    it('sets visibility for "Service Catalogs" and "Requests" only on navbar and enables "Service Request" button', function() {
+      var response = {authorization: {product_features: {
+        svc_catalog_provision: {},
+        miq_request_view: {}
+      }}};
+      gettextCatalog.loadAndSet = function() {};
+      $httpBackend.whenGET('/api?attributes=authorization').respond(response);
+      Session.loadUser();
+      $httpBackend.flush();
+
+      expect($state.navFeatures.dashboard.show).to.eq(true);
+      expect($state.navFeatures.services.show).to.eq(false);
+      expect($state.navFeatures.requests.show).to.eq(true);
+      expect($state.navFeatures.marketplace.show).to.eq(true);
+    });
+
+    it('returns false if user is not entitled to use ssui', function() {
+      var response = {authorization: {product_features: {
+      }}};
+      gettextCatalog.loadAndSet = function() {};
+      $httpBackend.whenGET('/api?attributes=authorization').respond(response);
+      Session.loadUser();
+      $httpBackend.flush();
+
+      expect(Session.activeNavigationFeatures()).to.eq(false);
     });
   });
 });
