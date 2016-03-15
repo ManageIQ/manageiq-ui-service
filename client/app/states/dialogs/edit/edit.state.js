4d7017192264e74f9a2ae835ff78da3c37c25957
Roman Blanco <rblanco@redhat.com>
2016-03-15 16:28:59 +0100

    [WIP] Save changes in dialog

--- client/app/states/dialogs/edit/edit.state.js
+++ client/app/states/dialogs/edit/edit.state.js
@@ -45,12 +45,48 @@
   }
 
   /** @ngInject */
-  function StateController($state, dialog, DialogEdit) {
+  function StateController($state, dialog, DialogEdit, CollectionsApi, Notifications) {
     var vm = this;
 
     DialogEdit.setData(dialog);
 
     vm.dialog = dialog;
-  }
+    vm.saveDialogDetails = saveDialogDetails;
+    vm.dismissChanges = dismissChanges;
+    vm.dialogUnchanged = dialogUnchanged;
 
+    function dismissChanges() {
+      $state.go('dialogs.details', {dialogId: dialog.id});
+    }
+
+    function saveDialogDetails() {
+      // TODO blindly-programmed, check after CRUD for service_dialog is done
+      // update tabs
+      DialogEdit.getData().content[0].dialog_tabs.forEach(function(tab) {
+        var dialogData = {
+          dialog_tabs: tab,
+        };
+
+        CollectionsApi.post(
+          'service_dialogs',
+          DialogEdit.getData().id,
+          {},
+          JSON.stringify({action: 'edit', resource: dialogData})
+        ).then(saveSuccess, saveFailure);
+      });
+    }
+
+    function dialogUnchanged() {
+      // TODO:
+    }
+
+    function saveSuccess() {
+      Notifications.success(vm.service.name + __(' was edited.'));
+      // TODO: $state.go($state.current, {}, {reload: true});
+    }
+
+    function saveFailure() {
+      Notifications.error(__('There was an error editing this dialog.'));
+    }
+  }
 })();
