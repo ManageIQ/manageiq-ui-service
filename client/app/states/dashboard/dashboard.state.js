c4ceeb9b3929fb05026cdf1ed49cc3330dfc4a23
Aparna Karve <akarve@redhat.com>
2016-05-12 12:44:46 -0700

    Added TODO notes

--- client/app/states/dashboard/dashboard.state.js
+++ client/app/states/dashboard/dashboard.state.js
@@ -34,6 +34,11 @@
   }
 
   /** @ngInject */
+  // TODO API in question: /api/requests
+  // TODO with filterValues = ['type=ServiceReconfigureRequest', 'or type=ServiceTemplateProvisionRequest', 'approval_state=pending_approval'];
+  // TODO API OR-ing bug/"design limitation" has forced an implementation change in how we gather count data for Requests
+  // TODO One API call would now be split into two - one for ServiceTemplateProvisionRequest and other for ServiceReconfigureRequest
+
   function resolvePendingRequests(CollectionsApi, $state) {
     if (!$state.navFeatures.requests.show) {
       return undefined;
