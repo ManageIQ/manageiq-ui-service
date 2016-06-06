0f6be7dfa35bd073c04ded744231674cdb6f4a98
David Taylor <dtaylor113@gmail.com>
2016-06-06 10:53:39 -0400

    SDUI: Fixed issue with Select Entry Point tree

---
+++ client/app/components/browse-entry-point-modal/browse-entry-point.directive.js
@@ -0,0 +1,41 @@
+(function() {
+  'use strict';
+
+  angular.module('app.components')
+    .directive('browseEntryPoint', browseEntryPointDirective);
+
+  /** @ngInject */
+  function browseEntryPointDirective() {
+    var directive = {
+      restrict: 'A',
+      link: link,
+    };
+
+    return directive;
+
+    function link(scope, element, attrs) {
+      $('#entryPointsTree').treeview({
+        collapseIcon: "fa fa-angle-down",
+        data: scope.vm.getTreeNodes(),
+        levels: 2,
+        expandIcon: "fa fa-angle-right",
+        nodeIcon: "fa fa-folder",
+        showBorder: false,
+        onNodeExpanded: function(event, node) {
+          scope.vm.onNodeExpanded(event, node);
+        }
+      });
+
+      // Get the inital root nodes
+      var i;
+      var rootNode = $('#entryPointsTree').treeview('getExpanded');
+
+      // Collapse all
+      $('#entryPointsTree').treeview('collapseAll');
+      // Auto-magically expand the root nodes
+      for (i = 0; i < rootNode.length; i++) {
+        $('#entryPointsTree').treeview('expandNode', [rootNode[i].nodeId]);
+      }
+    }
+  }
+})();
