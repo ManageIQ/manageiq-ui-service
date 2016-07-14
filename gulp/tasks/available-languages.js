bab771158f0f26a03229e7063f21a44cc0ce9a07
Martin Hradil <himdel@seznam.cz>
2016-07-14 15:13:05 +0200

    Merge pull request #77 from mzazrivec/ssui_run_time_detection_of_available_languages
    Gulp task 'available-languages' to create list of available languages
    (cherry picked from commit cda2f681c68bc317d407f41a5fcbf3674d2853c7)

---
+++ gulp/tasks/available-languages.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var glob = require('glob');
+var fs = require('fs');
+var log = require('../utils/log');
+
+module.exports = function(gulp, options) {
+  var config = require('../config')[options.key || 'availableLanguages'];
+
+  return task;
+
+  function task() {
+    if (options.verbose) {
+      log('Creating list of available languages under client/gettext/json');
+    }
+
+    var langFiles = glob.sync(config.catalogs);
+    var availableLanguages = {};
+    var supportedLanguages = [];
+
+    if (fs.existsSync(config.supportedLangsFile)) {
+      supportedLanguages = JSON.parse(fs.readFileSync(config.supportedLangsFile, 'utf8'));
+    }
+
+    langFiles.forEach(function(item, index) {
+      var catalog = JSON.parse(fs.readFileSync(item, 'utf8'));
+
+      for(var propName in catalog) {
+        if (typeof(catalog[propName]) != 'undefined') {
+          // If we have a list of supported languages and the language is not on the list, we skip it
+          if (supportedLanguages.length > 0 && !supportedLanguages.includes(propName)) {
+            continue;
+          }
+
+          if (typeof(catalog[propName]['locale_name']) != 'undefined') {
+            availableLanguages[propName] = catalog[propName]['locale_name'];
+          } else {
+            availableLanguages[propName] = '';
+          }
+        }
+      }
+    });
+
+    availableLanguages = JSON.stringify(availableLanguages);
+    fs.writeFileSync(config.availLangsFile, availableLanguages, {encoding: 'utf-8', flag: 'w+'});
+  }
+};
