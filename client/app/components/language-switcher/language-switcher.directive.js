28b561906ef0f86ac4b7d5ef4921b11f8a5e0b3c
Martin Hradil <mhradil@redhat.com>
2016-07-17 13:07:55 +0000

    LanguageSwitcher - add mode=select, use on login screen

--- client/app/components/language-switcher/language-switcher.directive.js
+++ client/app/components/language-switcher/language-switcher.directive.js
@@ -8,7 +8,9 @@
   function LanguageSwitcherDirective() {
     var directive = {
       restrict: 'AE',
-      scope: {},
+      scope: {
+        mode: '@?',
+      },
       templateUrl: 'app/components/language-switcher/language-switcher.html',
       controller: LanguageSwitcherController,
       controllerAs: 'vm',
@@ -20,11 +22,19 @@
     /** @ngInject */
     function LanguageSwitcherController(gettextCatalog, Language, lodash) {
       var vm = this;
+      vm.mode = vm.mode || 'menu';
 
       var hardcoded = {
+        "_user_": __("User Default"),
         "_browser_": __("Browser Default"),
       };
       vm.available = hardcoded;
+
+      if (vm.mode === 'menu') {
+        delete hardcoded._user_;
+      } else {
+        vm.chosen = Language.chosen = { code: '_user_' };
+      }
 
       Language.ready
         .then(function(available) {
