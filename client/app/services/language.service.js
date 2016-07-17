6822ac0f575708518f8f2e9ea08007c5f27bc58d
Martin Hradil <mhradil@redhat.com>
2016-07-17 13:02:22 +0000

    add LanguageSwitcher directive, load options from server

--- client/app/services/language.service.js
+++ client/app/services/language.service.js
@@ -5,22 +5,75 @@
     .factory('Language', LanguageFactory);
 
   /** @ngInject */
-  function LanguageFactory(gettextCatalog) {
+  function LanguageFactory($http, $q, gettextCatalog, lodash) {
+    var availableAvailable = $q.defer();
     var service = {
+      available: {
+        'en': 'English',  // not translated on purpose
+      },
+      ready: availableAvailable.promise,
       onLogin: onLogin,
       onReload: onReload,
+      setLocale: setLocale,
     };
+
+    init();
 
     return service;
 
+    function init() {
+      $http.get('gettext/json/available_languages.json')
+      .then(function(response) {
+        lodash.extend(service.available, response.data);
+        availableAvailable.resolve(service.available);
+      });
+    }
+
+    function setLocale(code) {
+      gettextCatalog.loadAndSet(code);
+
+      return code;
+    }
+
+    function getLocale(data) {
+      return data
+        && data.settings
+        && data.settings.locale;
+    }
+
     function onLogin(data) {
-      var locale = data.settings && data.settings.locale;
-      gettextCatalog.loadAndSet(locale);
+      var code = getLocale(data);
+      setLocale(code);
     }
 
     function onReload(data) {
-      var locale = data.settings && data.settings.locale;
-      gettextCatalog.loadAndSet(locale);
+      var code = getLocale(data);
+      setLocale(code);
+    }
+
+    function save(code) {
+      if (!service.user_href) {
+        console.error('Trying to save language selection without a valid user_href');
+
+        return;
+      }
+
+      if (code === '_browser_') {
+        code = null;
+      }
+
+      return $http.post(service.user_href, {
+        action: 'edit',
+        resource: {
+          settings: {
+            ui_self_service: {
+              display: {
+                locale: code,
+              },
+            },
+          },
+        },
+      });
     }
   }
 })();
