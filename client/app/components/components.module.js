220d4e4ae37957c13ff8bd6a39d5acdabd66a17c
Eric Winchell <ewinchel@redhat.com>
2016-09-07 13:00:23 -0400

    About Modal
    https://bugzilla.redhat.com/show_bug.cgi?id=1277207
    https://www.pivotaltracker.com/n/projects/1613907/stories/123185391

--- client/app/components/components.module.js
+++ client/app/components/components.module.js
@@ -9,4 +9,25 @@
     'ngSVGAttributes',
     'dndLists'
   ]);
+
+  angular.module('patternfly.modals').controller('ModalCtrl', function ($scope) {
+    $scope.additionalInfo = "Donec consequat dignissim neque, sed suscipit quam egestas in. Fusce bibendum " +
+      "laoreet lectus commodo interdum. Vestibulum odio ipsum, tristique et ante vel, iaculis placerat nulla. " +
+      "Suspendisse iaculis urna feugiat lorem semper, ut iaculis risus tempus.";
+    $scope.copyright = "Trademark and Copyright Information";
+    $scope.imgAlt = "Patternfly Symbol";
+    $scope.imgSrc = "images/login-screen-logo.png";
+    $scope.title = "Product Title";
+    $scope.productInfo = [
+      { name: 'Version', value: '1.0.0.0.20160819142038_51be77c' },
+      { name: 'Server Name', value: 'Localhost' },
+      { name: 'User Name', value: 'admin' },
+      { name: 'User Role', value: 'Administrator' }];
+    $scope.open = function () {
+      $scope.isOpen = true;
+    }
+    $scope.onClose = function() {
+      $scope.isOpen = false;
+    }
+  });
 })();
