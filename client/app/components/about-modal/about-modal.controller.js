1faaeed6c43998424fa36a93a5de3ac278a3da0d
Zita Nemeckova <znemecko@redhat.com>
2016-09-14 10:57:35 +0200

    Get rid of $scope

--- client/app/components/about-modal/about-modal.controller.js
+++ client/app/components/about-modal/about-modal.controller.js
@@ -1,25 +1,26 @@
 (function() {
   'use strict';
     
-  angular.module('app.components').controller('ModalCtrl', function ($scope, ServerInfo) {
-    $scope.additionalInfo = "";
-    $scope.copyright = "Copyright (c) 2016 ManageIQ. Sponsored by Red Hat Inc.";
-    $scope.imgAlt = "ManageIQ logo";
-    $scope.imgSrc = "images/login-screen-logo.png";
-    $scope.title = "ManageIQ UI Self Service";
+  angular.module('app.components').controller('ModalCtrl', function (ServerInfo) {
+    var controller = this;
+    this.additionalInfo = "";
+    this.copyright = "Copyright (c) 2016 ManageIQ. Sponsored by Red Hat Inc.";
+    this.imgAlt = "ManageIQ logo";
+    this.imgSrc = "images/login-screen-logo.png";
+    this.title = "ManageIQ UI Self Service";
     ServerInfo.promise.then( function(){
-      $scope.productInfo = [
+      controller.productInfo = [
         { name: 'Version', value: ServerInfo.data.version },
         { name: 'Server Name', value: ServerInfo.data.server },
         { name: 'User Name', value: ServerInfo.data.user },
         { name: 'User Role', value: ServerInfo.data.role },
       ];
     });
-    $scope.open = function () {
-      $scope.isOpen = true;
+    this.open = function () {
+      controller.isOpen = true;
     };
-    $scope.onClose = function() {
-      $scope.isOpen = false;
+    this.onClose = function() {
+      controller.isOpen = false;
     };
 });
 })();
