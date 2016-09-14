30c535bf653a2ab526e3d6c4a5d4217fbb350839
Zita Nemeckova <znemecko@redhat.com>
2016-09-14 12:30:46 +0200

    Remove ng-controller

--- client/app/components/about-modal/about-modal.controller.js
+++ client/app/components/about-modal/about-modal.controller.js
@@ -1,26 +1,34 @@
 (function() {
   'use strict';
     
-  angular.module('app.components').controller('ModalCtrl', function (ServerInfo) {
-    var controller = this;
-    this.additionalInfo = "";
-    this.copyright = "Copyright (c) 2016 ManageIQ. Sponsored by Red Hat Inc.";
-    this.imgAlt = "ManageIQ logo";
-    this.imgSrc = "images/login-screen-logo.png";
-    this.title = "ManageIQ UI Self Service";
-    ServerInfo.promise.then( function(){
-      controller.productInfo = [
-        { name: 'Version', value: ServerInfo.data.version },
-        { name: 'Server Name', value: ServerInfo.data.server },
-        { name: 'User Name', value: ServerInfo.data.user },
-        { name: 'User Role', value: ServerInfo.data.role },
-      ];
+  angular.module('app.components')
+    .controller('ModalCtrl', function (ServerInfo) {
+      var controller = this;
+      this.additionalInfo = "";
+      this.copyright = "Copyright (c) 2016 ManageIQ. Sponsored by Red Hat Inc.";
+      this.imgAlt = "ManageIQ logo";
+      this.imgSrc = "images/login-screen-logo.png";
+      this.title = "ManageIQ UI Self Service";
+      ServerInfo.promise.then( function(){
+        controller.productInfo = [
+          { name: 'Version', value: ServerInfo.data.version },
+          { name: 'Server Name', value: ServerInfo.data.server },
+          { name: 'User Name', value: ServerInfo.data.user },
+          { name: 'User Role', value: ServerInfo.data.role },
+        ];
+      });
+      this.open = function () {
+        controller.isOpen = true;
+      };
+      this.onClose = function() {
+        controller.isOpen = false;
+      };
+    })
+    .directive('aboutModal', function() {
+      return {
+        templateUrl: "app/components/about-modal/about-modal.html",
+        controller: "ModalCtrl",
+        controllerAs: "info",
+      };
     });
-    this.open = function () {
-      controller.isOpen = true;
-    };
-    this.onClose = function() {
-      controller.isOpen = false;
-    };
-});
 })();
