08545c15e48fb119939eb96303552dbf2d54d326
Allen Wight <allen.b.wight@gmail.com>
2016-09-15 14:01:56 -0400

    Resolve eslint dn-in error, removed unnecessary dependencies

--- client/app/states/designer/blueprints/editor/editorCtrl.js
+++ client/app/states/designer/blueprints/editor/editorCtrl.js
@@ -1,199 +1,205 @@
-angular.module('app.states')
-    .controller('blueprintEditorCtrl', ['$scope', '$timeout', 'BlueprintsState', 'DesignerState', 'BlueprintDetailsModal',
-      'SaveBlueprintModal', '$rootScope', '$state', 'CollectionsApi', 'Notifications', 'sprintf',
-      function($scope, $timeout, BlueprintsState, DesignerState, BlueprintDetailsModal, SaveBlueprintModal, $rootScope, $state, // jshint ignore:line
-               CollectionsApi, Notifications, sprintf) {
-        $scope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
-          if (toState.name === 'login') {
-            return;
-          }
-          if (fromState.name === "designer.blueprints.editor" && toState.name !== "designer.blueprints.editor") {
-            var origBlueprint = BlueprintsState.getOriginalBlueprint();
-            if (!BlueprintsState.doNotSave() && !angular.equals(origBlueprint, $scope.blueprint) && !event.defaultPrevented) {
-              SaveBlueprintModal.showModal($scope.blueprint, toState, toParams, fromState, fromParams);
-              event.preventDefault();
-            }
-          }
-        });
+(function() {
+  'use strict';
 
-        $scope.blueprint = $scope.$parent.vm.blueprint;
+  angular.module('app.states')
+    .controller('BlueprintEditorCtrl', ['$scope', '$timeout', 'BlueprintsState', 'DesignerState', 'BlueprintDetailsModal',
+      'SaveBlueprintModal', '$state', 'Notifications', 'sprintf', ComponentController]);
 
-        var blueprintDirty = false;
+  /** @ngInject */
+  function ComponentController($scope, $timeout, BlueprintsState, DesignerState, BlueprintDetailsModal, SaveBlueprintModal, $state,
+                               Notifications, sprintf) {
+    $scope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+      if (toState.name === 'login') {
+        return;
+      }
+      if (fromState.name === "designer.blueprints.editor" && toState.name !== "designer.blueprints.editor") {
+        var origBlueprint = BlueprintsState.getOriginalBlueprint();
+        if (!BlueprintsState.doNotSave() && !angular.equals(origBlueprint, $scope.blueprint) && !event.defaultPrevented) {
+          SaveBlueprintModal.showModal($scope.blueprint, toState, toParams, fromState, fromParams);
+          event.preventDefault();
+        }
+      }
+    });
 
-        if (!$scope.blueprint) {
-          $scope.blueprint = angular.copy(newBlueprint());
+    $scope.blueprint = $scope.$parent.vm.blueprint;
+
+    var blueprintDirty = false;
+
+    if (!$scope.blueprint) {
+      $scope.blueprint = angular.copy(newBlueprint());
+    }
+
+    BlueprintsState.saveOriginalBlueprint(angular.copy($scope.blueprint));
+
+    // console.log("RETRIEVED Blueprint: " + angular.toJson($scope.blueprint, true));
+
+    $scope.$watch("blueprint", function(oldValue, newValue) {
+      if (!angular.equals(oldValue, newValue, true)) {
+        blueprintDirty = true;
+      }
+    }, true);
+
+    $scope.$on('BlueprintCanvasChanged', function(evt, args) {
+      if (args.chartDataModel && !angular.equals($scope.blueprint.ui_properties.chartDataModel, args.chartDataMode)) {
+        $scope.blueprint.ui_properties.chartDataModel = args.chartDataModel;
+        blueprintDirty = true;
+      }
+    });
+
+    $scope.blueprintUnchanged = function() {
+      return !blueprintDirty;
+    };
+
+    function newBlueprint() {
+      var blueprint = BlueprintsState.getNewBlueprintObj();
+      blueprintDirty = true;
+
+      return blueprint;
+    }
+
+    $scope.saveBlueprint = function() {
+      BlueprintsState.saveBlueprint($scope.blueprint).then(saveSuccess, saveFailure);
+
+      function saveSuccess(id) {
+        Notifications.success(sprintf(__("'%s' was succesfully saved."), $scope.blueprint.name));
+        if (id) {
+          $state.go($state.current, {blueprintId: id}, {reload: true});
         }
 
-        BlueprintsState.saveOriginalBlueprint(angular.copy($scope.blueprint));
+        /*
+         Saving in case we decide to not always reload; for performance
 
-        // console.log("RETRIEVED Blueprint: " + angular.toJson($scope.blueprint, true));
+         get another copy to work, different obj from what was saved
+         BlueprintsState.saveOriginalBlueprint(angular.copy($scope.blueprint));
+         blueprintDirty = false;
+         $( "#saveBtm" ).blur();
+         console.log("Saved blueprint from designer"); */
+      }
 
-        $scope.$watch("blueprint", function(oldValue, newValue) {
-          if (!angular.equals(oldValue, newValue, true)) {
-            blueprintDirty = true;
-          }
-        }, true);
+      function saveFailure() {
+        Notifications.error(sprintf(__("Failed to save '%s'."), $scope.blueprint.name));
+      }
+    };
 
-        $scope.$on('BlueprintCanvasChanged', function(evt, args) {
-          if (args.chartDataModel && !angular.equals($scope.blueprint.ui_properties.chartDataModel, args.chartDataMode)) {
-            $scope.blueprint.ui_properties.chartDataModel = args.chartDataModel;
-            blueprintDirty = true;
-          }
+    $scope.editDetails = function() {
+      $scope.loading = true;
+      BlueprintDetailsModal.showModal('edit', $scope.blueprint).then(
+        function() {
+          $scope.loading = false;
         });
+    };
 
-        $scope.blueprintUnchanged = function() {
-          return !blueprintDirty;
-        };
+    $scope.itemsSelected = function() {
+      if ($scope.chartViewModel && $scope.chartViewModel.getSelectedNodes) {
+        return $scope.chartViewModel.getSelectedNodes().length > 0;
+      } else {
+        return false;
+      }
+    };
 
-        function newBlueprint() {
-          var blueprint = BlueprintsState.getNewBlueprintObj();
-          blueprintDirty = true;
+    $scope.onlyOneTtemSelected = function() {
+      if ($scope.chartViewModel && $scope.chartViewModel.getSelectedNodes) {
+        return $scope.chartViewModel.getSelectedNodes().length === 1;
+      } else {
+        return false;
+      }
+    };
 
-          return blueprint;
-        }
+    $scope.duplicateSelectedItem = function() {
+      $scope.$broadcast('duplicateSelectedItem');
+      $("#duplicateItem").blur();
+    };
 
-        $scope.saveBlueprint = function() {
-          BlueprintsState.saveBlueprint($scope.blueprint).then(saveSuccess, saveFailure);
+    $scope.removeSelectedItemsFromCanvas = function() {
+      $scope.$broadcast('removeSelectedItems');
+      $("#removeItems").blur();
+    };
 
-          function saveSuccess(id) {
-            Notifications.success(sprintf(__("'%s' was succesfully saved."), $scope.blueprint.name));
-            if (id) {
-              $state.go($state.current, {blueprintId: id}, {reload: true});
-            }
+    /*  Catalog Editor Toolbox Methods */
 
-            /*
-             Saving in case we decide to not always reload; for performance
+    $scope.toolboxVisible = false;
 
-             get another copy to work, different obj from what was saved
-             BlueprintsState.saveOriginalBlueprint(angular.copy($scope.blueprint));
-             blueprintDirty = false;
-             $( "#saveBtm" ).blur();
-             console.log("Saved blueprint from designer"); */
-          }
+    $scope.showToolbox = function() {
+      $scope.toolboxVisible = true;
+      // add class to subtabs to apply PF style and
+      // focus to filter input box
 
-          function saveFailure() {
-            Notifications.error(sprintf(__("Failed to save '%s'."), $scope.blueprint.name));
-          }
-        };
+      $timeout(function() {
+        $(".subtabs>ul").addClass('nav-tabs-pf');
+        $("#filterFld").focus();
+      });
+    };
 
-        $scope.editDetails = function() {
-          $scope.loading = true;
-          BlueprintDetailsModal.showModal('edit', $scope.blueprint).then(
-              function() {
-                $scope.loading = false;
-              });
-        };
+    $scope.hideToolbox = function() {
+      $scope.toolboxVisible = false;
+    };
 
-        $scope.itemsSelected = function() {
-          if ($scope.chartViewModel && $scope.chartViewModel.getSelectedNodes) {
-            return $scope.chartViewModel.getSelectedNodes().length > 0;
-          } else {
-            return false;
-          }
-        };
+    $scope.$on('clickOnChart', function(evt) {
+      $scope.hideToolbox();
+    });
 
-        $scope.onlyOneTtemSelected = function() {
-          if ($scope.chartViewModel && $scope.chartViewModel.getSelectedNodes) {
-            return $scope.chartViewModel.getSelectedNodes().length === 1;
-          } else {
-            return false;
-          }
-        };
+    $scope.tabClicked = function() {
+      $("#filterFld").focus();
+    };
 
-        $scope.duplicateSelectedItem = function() {
-          $scope.$broadcast('duplicateSelectedItem');
-          $( "#duplicateItem" ).blur();
-        };
+    $scope.inConnectingMode = false;
+    $scope.hideConnectors = false;
 
-        $scope.removeSelectedItemsFromCanvas = function() {
-          $scope.$broadcast('removeSelectedItems');
-          $( "#removeItems" ).blur();
-        };
+    // broadcast hideConnectors change down to canvas
+    $scope.toggleshowHideConnectors = function() {
+      $scope.$broadcast('hideConnectors', {hideConnectors: $scope.hideConnectors});
+    };
 
-        /*  Catalog Editor Toolbox Methods */
+    // listen for in connecting mode from canvas
+    $scope.$on('inConnectingMode', function(evt, args) {
+      $scope.inConnectingMode = args.inConnectingMode;
+      $scope.hideConnectors = false;
+    });
 
-        $scope.toolboxVisible = false;
+    $scope.tabs = DesignerState.getDesignerToolboxTabs($scope.$parent.vm.serviceTemplates);
 
-        $scope.showToolbox = function() {
-          $scope.toolboxVisible = true;
-          // add class to subtabs to apply PF style and
-          // focus to filter input box
+    $scope.getNewItem = function() {
+      var activeTab = $scope.activeTab();
+      var activeSubTab = $scope.activeSubTab();
+      var activeSubSubTab = $scope.activeSubSubTab();
 
-          $timeout(function() {
-            $( ".subtabs>ul" ).addClass('nav-tabs-pf');
-            $( "#filterFld" ).focus();
-          });
-        };
+      if (activeSubSubTab && activeSubSubTab.newItem) {
+        return activeSubSubTab.newItem;
+      }
 
-        $scope.hideToolbox = function() {
-          $scope.toolboxVisible = false;
-        };
+      if (activeSubTab && activeSubTab.newItem) {
+        return activeSubTab.newItem;
+      }
 
-        $scope.$on('clickOnChart', function(evt) {
-          $scope.hideToolbox();
-        });
+      if (activeTab && activeTab.newItem) {
+        return activeTab.newItem;
+      }
 
-        $scope.tabClicked = function() {
-          $( "#filterFld" ).focus();
-        };
+      return {name: 'New Item', image: 'images/service.png'};
+    };
 
-        $scope.inConnectingMode = false;
-        $scope.hideConnectors = false;
+    $scope.activeTab = function() {
+      return $scope.tabs.filter(function(tab) {
+        return tab.active;
+      })[0];
+    };
 
-        // broadcast hideConnectors change down to canvas
-        $scope.toggleshowHideConnectors = function() {
-          $scope.$broadcast('hideConnectors', {hideConnectors: $scope.hideConnectors});
-        };
+    $scope.activeSubTab = function() {
+      var activeTab = $scope.activeTab();
+      if (activeTab && activeTab.subtabs) {
+        return activeTab.subtabs.filter(function(subtab) {
+          return subtab.active;
+        })[0];
+      }
+    };
 
-        // listen for in connecting mode from canvas
-        $scope.$on('inConnectingMode', function(evt, args) {
-          $scope.inConnectingMode = args.inConnectingMode;
-          $scope.hideConnectors = false;
-        });
-
-        $scope.tabs = DesignerState.getDesignerToolboxTabs($scope.$parent.vm.serviceTemplates);
-
-        $scope.getNewItem = function() {
-          var activeTab = $scope.activeTab();
-          var activeSubTab = $scope.activeSubTab();
-          var activeSubSubTab = $scope.activeSubSubTab();
-
-          if (activeSubSubTab && activeSubSubTab.newItem) {
-            return activeSubSubTab.newItem;
-          }
-
-          if (activeSubTab && activeSubTab.newItem) {
-            return activeSubTab.newItem;
-          }
-
-          if (activeTab && activeTab.newItem) {
-            return activeTab.newItem;
-          }
-
-          return {name: 'New Item', image: 'images/service.png'};
-        };
-
-        $scope.activeTab = function() {
-          return $scope.tabs.filter(function(tab) {
-            return tab.active;
-          })[0];
-        };
-
-        $scope.activeSubTab = function() {
-          var activeTab = $scope.activeTab();
-          if (activeTab && activeTab.subtabs) {
-            return activeTab.subtabs.filter(function(subtab) {
-              return subtab.active;
-            })[0];
-          }
-        };
-
-        $scope.activeSubSubTab = function() {
-          var activeSubTab = $scope.activeSubTab();
-          if (activeSubTab && activeSubTab.subtabs) {
-            return activeSubTab.subtabs.filter(function(subsubtab) {
-              return subsubtab.active;
-            })[0];
-          }
-        };
-      }]);
+    $scope.activeSubSubTab = function() {
+      var activeSubTab = $scope.activeSubTab();
+      if (activeSubTab && activeSubTab.subtabs) {
+        return activeSubTab.subtabs.filter(function(subsubtab) {
+          return subsubtab.active;
+        })[0];
+      }
+    };
+  }
+})();
