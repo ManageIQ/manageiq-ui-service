acbc7b9cad7dfb2d02b7f7eab212ad7cc48d0cbd
Zita Nemeckova <znemecko@redhat.com>
2016-09-22 18:25:27 +0200

    Fix tests and style

--- client/app/components/about-modal/about-modal.controller.js
+++ client/app/components/about-modal/about-modal.controller.js
@@ -2,34 +2,34 @@
   'use strict';
     
   angular.module('app.components')
-      .directive('aboutModal', function() {
-          return {
-              templateUrl: "app/components/about-modal/about-modal.html",
-              controller: ModalCtrl,
-              controllerAs: "info",
-          };
-      });
+    .directive('aboutModal', function() {
+      return {
+        templateUrl: "app/components/about-modal/about-modal.html",
+        controller: ModalCtrl,
+        controllerAs: "info",
+      };
+    });
   /** @ngInject */
   function ModalCtrl(ServerInfo) {
-      var controller = this;
-      this.additionalInfo = "";
-      this.copyright = "Copyright (c) 2016 ManageIQ. Sponsored by Red Hat Inc.";
-      this.imgAlt = "ManageIQ logo";
-      this.imgSrc = "images/login-screen-logo.png";
-      this.title = "ManageIQ UI Self Service";
-      ServerInfo.promise.then( function(){
-        controller.productInfo = [
-          { name: 'Version', value: ServerInfo.data.version },
-          { name: 'Server Name', value: ServerInfo.data.server },
-          { name: 'User Name', value: ServerInfo.data.user },
-          { name: 'User Role', value: ServerInfo.data.role },
-        ];
-      });
-      this.open = function () {
-        controller.isOpen = true;
-      };
-      this.onClose = function() {
-        controller.isOpen = false;
-      };
-    }
+    var vm = this;
+    this.additionalInfo = "";
+    this.copyright = "Copyright (c) 2016 ManageIQ. Sponsored by Red Hat Inc.";
+    this.imgAlt = "ManageIQ logo";
+    this.imgSrc = "images/login-screen-logo.png";
+    this.title = "ManageIQ UI Self Service";
+    ServerInfo.promise.then( function() {
+      vm.productInfo = [
+        { name: 'Version', value: ServerInfo.data.version },
+        { name: 'Server Name', value: ServerInfo.data.server },
+        { name: 'User Name', value: ServerInfo.data.user },
+        { name: 'User Role', value: ServerInfo.data.role },
+      ];
+    });
+    this.open = function() {
+      vm.isOpen = true;
+    };
+    this.onClose = function() {
+      vm.isOpen = false;
+    };
+  }
 })();
--- client/app/services/server-info.service.js
+++ client/app/services/server-info.service.js
@@ -4,21 +4,22 @@
   angular.module('app.services')
     .service('ServerInfo', ServerInfo);
 
-    /** @ngInject */
-    function ServerInfo($q) {
-      this.data = {};
-      var self = this;
-      this.promise = $q(function(resolve, reject) {
-        self.set = function(data){
-          self.data = {
-            user: data.identity.name,
-            role: data.identity.role,
-            version: data.server_info.version + '.' + data.server_info.build,
-            server: data.server_info.appliance,
-          };
-          resolve(self.data);
-          return data;
+  /** @ngInject */
+  function ServerInfo($q) {
+    this.data = {};
+    var self = this;
+    this.promise = $q(function(resolve, reject) {
+      self.set = function(data) {
+        self.data = {
+          user: data.identity.name,
+          role: data.identity.role,
+          version: data.server_info.version + '.' + data.server_info.build,
+          server: data.server_info.appliance,
         };
-      });
-    }
+        resolve(self.data);
+        
+        return data;
+      };
+    });
+  }
 })();
