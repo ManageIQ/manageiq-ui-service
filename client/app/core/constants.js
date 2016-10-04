fb4ff17c936be07e0c29cec7e6fcbc781a26f42c
Dávid Halász <dhalasz@redhat.com>
2016-10-04 13:54:43 +0200

    Display notifications arriving through ActionCable in the drawer
    (cherry picked from commit 0067e2e4cc80819a0f879647c5d0242d0254b2b4)

--- client/app/core/constants.js
+++ client/app/core/constants.js
@@ -1,9 +1,10 @@
-/* global toastr:false, _:false, $:false, sprintf: false */
+/* global toastr:false, _:false, ActionCable:false, $:false, sprintf: false */
 (function() {
   'use strict';
 
   angular.module('app.core')
     .constant('lodash', _)
+    .constant('ActionCable', ActionCable)
     .constant('toastr', toastr)
     .constant('sprintf', sprintf);
 })();
