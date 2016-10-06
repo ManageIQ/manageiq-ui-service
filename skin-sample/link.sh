37363d05482060d7147dbc6d7b46ea7da80e0bf4
Martin Hradil <mhradil@redhat.com>
2016-10-06 12:59:05 +0000

    Skinning - scripts to enable/disable the skin

---
+++ skin-sample/link.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SOURCE=""
+TARGET=""
+
+cd "`dirname "$0"`"
+SOURCE=`pwd`
+cd -
+
+if [ -n "$1" ]; then
+  TARGET="$1"
+  if ! [ -d "$1" ]; then
+    echo "Target ($1) is not a directory" 1>&2
+    exit 1
+  fi
+else
+  cd "`dirname "$0"`"/..
+  TARGET=`pwd`
+  cd -
+fi
+
+if ! [ -d "$TARGET"/client -a -d "$TARGET"/server ]; then
+  echo "Target ($TARGET) is not the Service UI directory (missing client/ or server/)" 1>&2
+  exit 1
+fi
+
+if [ -e "$TARGET"/client/skin ]; then
+  echo "Target ($TARGET) is already skinned" 1>&2
+  ls -ld "$TARGET"/client/skin
+  exit 2
+fi
+
+ln -vsf "$SOURCE" "$TARGET"/client/skin
