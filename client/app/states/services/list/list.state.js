4b5936ad8e7374f030f0f81fba1dff2312697128
Martin Hradil <mhradil@redhat.com>
2016-10-23 20:18:08 +0000

    services.details - Monthly Charge in the detail

--- client/app/states/services/list/list.state.js
+++ client/app/states/services/list/list.state.js
@@ -52,7 +52,7 @@
       }
     });
 
-    vm.services.forEach(Chargeback.addListData);
+    vm.services.forEach(Chargeback.processReports);
     Chargeback.adjustRelativeCost(vm.services);
 
     vm.servicesList = angular.copy(vm.services);
