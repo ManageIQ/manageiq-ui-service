bc38559c10566fb7f2feeca521cf74c4291610c3
Martin Hradil <mhradil@redhat.com>
2016-10-23 19:04:21 +0000

    services.list - sort & filter by relative cost
    also added missing N_ around filter fields

--- client/app/states/services/list/list.state.js
+++ client/app/states/services/list/list.state.js
@@ -98,6 +98,13 @@
             placeholder: __('Filter by Created On'),
             filterType: 'text',
           },
+          {
+            id: 'chargeback_relative_cost',
+            title: __('Relative Cost'),
+            placeholder: __('Filter by Relative Cost'),
+            filterType: 'select',
+            filterValues: ['$', '$$', '$$$', '$$$$'],
+          },
         ],
         resultsCount: vm.servicesList.length,
         appliedFilters: ServicesState.filterApplied ? ServicesState.getFilters() : [],
@@ -129,6 +136,11 @@
             id: 'created',
             title: __('Created'),
             sortType: 'numeric',
+          },
+          {
+            id: 'chargeback_relative_cost',
+            title: __('Relative Cost'),
+            sortType: 'alpha',
           },
         ],
         onSortChange: sortChange,
@@ -179,6 +191,8 @@
         compValue = new Date(item1.created_at) - new Date(item2.created_at);
       } else if (vm.toolbarConfig.sortConfig.currentField.id === 'retires') {
         compValue = getRetirementDate(item1.retires_on) - getRetirementDate(item2.retires_on);
+      } else if (vm.toolbarConfig.sortConfig.currentField.id === 'chargeback_relative_cost') {
+        compValue = item1.chargeback_relative_cost.length - item2.chargeback_relative_cost.length;
       }
 
       if (!vm.toolbarConfig.sortConfig.isAscending) {
@@ -252,6 +266,8 @@
         return checkRetirementDate(item, filter.value.toLowerCase());
       } else if (filter.id === 'created') {
         return $filter('date')(item.created_at).toLowerCase().indexOf(filter.value.toLowerCase()) !== -1;
+      } else if (filter.id === 'chargeback_relative_cost') {
+        return item.chargeback_relative_cost === filter.value;
       }
 
       return false;
