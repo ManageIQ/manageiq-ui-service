aed225fed463b1688402d7f2c525fbf16e072a28
Allen Wight <allen.b.wight@gmail.com>
2016-11-21 13:36:24 -0500

    Add loading component explorer and all future lists that pull from api

---
+++ client/app/components/loading/loading.component.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular.module('app.components')
+    .component('loading', {
+      bindings: {
+        status: '<',
+      },
+      template: '<div ng-if="$ctrl.status" class="drawer-pf-loading text-center">' +
+      '<span class="spinner spinner-xs spinner-inline"></span> {{ "Loading More" | translate }}  </div>',
+    });
+})();
