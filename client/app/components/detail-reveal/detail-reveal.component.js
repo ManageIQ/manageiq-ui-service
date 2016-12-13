f9489f59054ec469cb13da6f015a91d37dd77d6a
Chris Hale <chris.hale@me.com>
2016-12-13 09:27:35 -0700

    Added new component and updated vm details screen

---
+++ client/app/components/detail-reveal/detail-reveal.component.js
@@ -0,0 +1,32 @@
+(function() {
+  'use strict';
+
+  angular.module('app.components')
+    .component('detailReveal', {
+      controller: ComponentController,
+      controllerAs: 'vm',
+      bindings: {
+        title: '@',
+        detail: '@',
+        icon: '@',
+        translateTitle: '=',
+      },
+      transclude: true,
+      templateUrl: 'app/components/detail-reveal/detail-reveal.html',
+    });
+
+  /** @ngInject */
+  function ComponentController($transclude) {
+    var vm = this;
+    vm.translateTitle = (angular.isDefined(vm.translateTitle) ? true : vm.translateTitle);
+    vm.title = (vm.translateTitle === true ? __(vm.title) : vm.title);
+
+    vm.$onInit = activate();
+
+    function activate() {
+      vm.toggleDetails = false;
+      vm.hasMoreDetails = $transclude().length > 0;
+      console.log(vm.hasMoreDetails);
+    }
+  }
+})();
