0adfde8cf351db0827a5502367ae2697274912eb
Allen Wight <allen.b.wight@gmail.com>
2016-12-13 10:39:03 -0500

    Pagination component support ang 1.6

--- client/app/components/pagination/pagination.component.js
+++ client/app/components/pagination/pagination.component.js
@@ -17,17 +17,17 @@
   function ComponentController() {
     var vm = this;
 
-    angular.extend(vm, {
-      leftBoundary: 0,
-      rightBoundary: 0,
-      offset: 0,
-      lastOffset: Math.floor(vm.count / vm.limit) * vm.limit,
-      disabled: disabled,
-      previous: previous,
-      next: next,
-    });
-
     vm.$onInit = function() {
+      angular.extend(vm, {
+        leftBoundary: 0,
+        rightBoundary: 0,
+        offset: 0,
+        lastOffset: Math.floor(vm.count / vm.limit) * vm.limit,
+        disabled: disabled,
+        previous: previous,
+        next: next,
+      });
+
       establishBoundaries();
     };
 
