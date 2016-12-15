eec09b9f3b5cb33dac1411aa629daeb7e2ca3d94
Jeffrey Phillips <jephilli@redhat.com>
2016-12-15 10:20:40 -0500

    Add Dual Pane Selector Component

---
+++ client/app/components/dual-pane-selector/dual-pane-selector.component.js
@@ -0,0 +1,139 @@
+/* eslint camelcase: "off" */
+(function() {
+  'use strict';
+
+  angular.module('app.components')
+    .component('dualPaneSelector', {
+      templateUrl: "app/components/dual-pane-selector/dual-pane-selector.html",
+      bindings: {
+        availableItems: "=",
+        selectedItems: "=",
+        availableTitle: "@",
+        selectedTitle: "@",
+        sortFn: '=',
+        onChangeFn: '=',
+      },
+      controller: DualPaneSelectorController,
+      controllerAs: 'vm',
+      replace: true,
+      transclude: {
+        available: '?available',
+        selected: '?selected',
+      },
+    });
+
+  /** @ngInject */
+  function DualPaneSelectorController(sprintf) {
+    var vm = this;
+
+    vm.$onInit = function() {
+      angular.extend(vm,
+        {
+          selectedAvailableItems: [],
+          selectedSelectedItems: [],
+          availableCountText: '',
+          selectedCountText: '',
+          isAvailableItemSelected: isAvailableItemSelected,
+          isSelectedItemSelected: isSelectedItemSelected,
+          availableItemClick: availableItemClick,
+          availableItemDoubleClick: availableItemDoubleClick,
+          selectedItemClick: selectedItemClick,
+          selectedItemDoubleClick: selectedItemDoubleClick,
+          selectItems: selectItems,
+          unSelectItems: unSelectItems,
+        }
+      );
+      vm.sortFn(vm.availableItems);
+      vm.sortFn(vm.selectedItems);
+
+      updateCountTexts();
+    };
+
+    function updateCountTexts() {
+      vm.availableCountText = sprintf(__("%d of %d items selected"),
+                                      vm.selectedAvailableItems.length,
+                                      vm.availableItems.length);
+      vm.selectedCountText = sprintf(__("%d of %d items selected"),
+                                     vm.selectedSelectedItems.length,
+                                     vm.selectedItems.length);
+    }
+
+    function handleListChange() {
+      updateCountTexts();
+      if (angular.isFunction(vm.onChangeFn)) {
+        vm.onChangeFn();
+      }
+    }
+
+    function isAvailableItemSelected(item) {
+      return vm.selectedAvailableItems.indexOf(item) !== -1;
+    }
+
+    function isSelectedItemSelected(item) {
+      return vm.selectedSelectedItems.indexOf(item) !== -1;
+    }
+
+    function toggleItemSelection(item, selectionList) {
+      var index = selectionList.indexOf(item);
+      if (index === -1) {
+        selectionList.push(item);
+      } else {
+        selectionList.splice(index, 1);
+      }
+      updateCountTexts();
+    }
+
+    function moveItem(item, fromList, toList, selectionList) {
+      var index = fromList.indexOf(item);
+      if (index !== -1) {
+        fromList.splice(index, 1);
+        toList.push(item);
+        vm.sortFn(toList);
+
+        index = selectionList.indexOf(item);
+        if (index !== -1) {
+          selectionList.splice(index, 1);
+        }
+      }
+      handleListChange();
+    }
+
+    function moveSelections(selectionList, fromList, toList) {
+      angular.forEach(selectionList, function(nextItem) {
+        toList.push(nextItem);
+        fromList.splice(fromList.indexOf(nextItem), 1);
+      });
+      vm.sortFn(fromList);
+      vm.sortFn(toList);
+
+      selectionList.splice(0, selectionList.length);
+
+      handleListChange();
+    }
+
+    function availableItemClick(item) {
+      toggleItemSelection(item, vm.selectedAvailableItems);
+    }
+
+    function availableItemDoubleClick(item) {
+      moveItem(item, vm.availableItems, vm.selectedItems, vm.selectedAvailableItems);
+    }
+
+    function selectedItemClick(item) {
+      toggleItemSelection(item, vm.selectedSelectedItems);
+    }
+
+    function selectedItemDoubleClick(item) {
+      moveItem(item, vm.selectedItems, vm.availableItems, vm.selectedSelectedItems);
+    }
+
+    function selectItems() {
+      moveSelections(vm.selectedAvailableItems, vm.availableItems, vm.selectedItems);
+    }
+
+    function unSelectItems() {
+      moveSelections(vm.selectedSelectedItems, vm.selectedItems, vm.availableItems);
+    }
+  }
+})();
+
---
+++ tests/components/dual-pane-selector.component.spec.js
@@ -0,0 +1,117 @@
+describe('Dual Pane Selector Component', function() {
+  beforeEach(module('app.components', 'gettext'));
+
+  describe('controller', function() {
+    var controller;
+    var $componentController;
+    var sortCount;
+    var changeCount;
+    var bindings;
+
+    beforeEach(inject(function(_$componentController_) {
+      sortCount = 0;
+      var sortFunction = function(items) {
+        sortCount++;
+      };
+
+      changeCount = 0;
+      var onChangeFunction = function() {
+        changeCount++;
+      };
+
+      bindings = {
+        availableItems: ['available1', 'available2', 'available3', 'available4'],
+        selectedItems: ['selected1', 'selected2'],
+        selectedTitle: 'Test Selected',
+        availableTitle: 'Test Available',
+        sortFn: sortFunction,
+        onChangeFn: onChangeFunction,
+      };
+
+      $componentController = _$componentController_;
+      controller = $componentController('dualPaneSelector', null, bindings);
+    }));
+
+    it('is defined and accepts bindings', function() {
+      expect(controller).to.be.defined;
+      expect(controller.selectedItems.length).to.equal(2);
+      expect(controller.availableItems.length).to.equal(4);
+    });
+
+    it('sorts both lists on initialization', function() {
+      controller.$onInit();
+      expect(sortCount).to.equal(2);
+    });
+
+    it('selects items on click', function() {
+      controller.$onInit();
+
+      expect(controller.isAvailableItemSelected(bindings.availableItems[0])).to.eq(false);
+      controller.availableItemClick(bindings.availableItems[0]);
+      expect(controller.isAvailableItemSelected(bindings.availableItems[0])).to.eq(true);
+
+      expect(controller.isSelectedItemSelected(bindings.selectedItems[0])).to.eq(false);
+      controller.selectedItemClick(bindings.selectedItems[0]);
+      expect(controller.isSelectedItemSelected(bindings.selectedItems[0])).to.eq(true);
+    });
+
+    it('sets the list count texts appropriately', function() {
+      controller.$onInit();
+      expect(controller.availableCountText).to.eq("0 of 4 items selected");
+      expect(controller.selectedCountText).to.eq("0 of 2 items selected");
+
+      controller.availableItemClick(bindings.availableItems[2]);
+      expect(controller.availableCountText).to.eq("1 of 4 items selected");
+
+      controller.selectedItemClick(bindings.selectedItems[1]);
+      expect(controller.selectedCountText).to.eq("1 of 2 items selected");
+    });
+
+    it('moves items on double click', function() {
+      controller.$onInit();
+
+      expect(controller.selectedItems.length).to.equal(2);
+      expect(controller.availableItems.length).to.equal(4);
+
+      controller.availableItemDoubleClick(bindings.availableItems[0]);
+
+      expect(controller.selectedItems.length).to.equal(3);
+      expect(controller.availableItems.length).to.equal(3);
+      expect(sortCount).to.equal(3);
+      expect(changeCount).to.equal(1);
+
+      controller.selectedItemDoubleClick(bindings.selectedItems[0]);
+
+      expect(controller.selectedItems.length).to.equal(2);
+      expect(controller.availableItems.length).to.equal(4);
+      expect(sortCount).to.equal(4);
+      expect(changeCount).to.equal(2);
+    });
+
+    it('moves items on move button clicks', function() {
+      controller.$onInit();
+
+      expect(controller.selectedItems.length).to.equal(2);
+      expect(controller.availableItems.length).to.equal(4);
+
+      controller.availableItemClick(bindings.availableItems[0]);
+      controller.availableItemClick(bindings.availableItems[1]);
+      controller.selectItems();
+
+      expect(controller.selectedItems.length).to.equal(4);
+      expect(controller.availableItems.length).to.equal(2);
+      expect(sortCount).to.equal(4);
+      expect(changeCount).to.equal(1);
+
+      controller.selectedItemClick(bindings.selectedItems[0]);
+      controller.selectedItemClick(bindings.selectedItems[1]);
+      controller.selectedItemClick(bindings.selectedItems[2]);
+      controller.unSelectItems();
+
+      expect(controller.selectedItems.length).to.equal(1);
+      expect(controller.availableItems.length).to.equal(5);
+      expect(sortCount).to.equal(6);
+      expect(changeCount).to.equal(2);
+    });
+  });
+});
