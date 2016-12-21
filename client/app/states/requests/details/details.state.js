0a3237e20576d6bd89c8b109dcd298e25daa2f9f
Chris Hale <chris.hale@me.com>
2016-12-21 14:02:21 -0700

    Added ability to edit a request

--- client/app/states/requests/details/details.state.js
+++ client/app/states/requests/details/details.state.js
@@ -32,10 +32,60 @@
   }
 
   /** @ngInject */
-  function RequestDetailsController(request) {
+  function RequestDetailsController(request, CollectionsApi, DialogFieldRefresh, EventNotifications) {
     var vm = this;
+    vm.editingDisabled = true;
 
     vm.title = request.description;
     vm.request = request;
+    vm.dialogs = [];
+    vm.dialogs.push(request.provision_dialog);
+
+    var autoRefreshableDialogFields = [];
+    var allDialogFields = [];
+
+    DialogFieldRefresh.setupDialogData(vm.dialogs, allDialogFields, autoRefreshableDialogFields);
+
+    var dialogUrl = 'service_catalogs/' + vm.request.source_id + '/service_templates';
+
+    angular.forEach(allDialogFields, function(dialogField) {
+      dialogField.refreshSingleDialogField = function() {
+        DialogFieldRefresh.refreshSingleDialogField(allDialogFields, dialogField, dialogUrl, vm.request.source_id);
+      };
+    });
+
+    function saveRequest() {
+      var dialogData = dataForSubmit();
+      var data = { action: 'edit', dialogData };
+      CollectionsApi.post(
+        'service_requests',
+        request.id,
+        {},
+        { action: 'edit', options: { dialog: dialogData } }
+      ).then(submitSuccess, submitFailure);
+    }
+    function dataForSubmit() {
+      var dialogFieldData = {};
+      angular.forEach(allDialogFields, function(dialogField) {
+        if ((dialogField.type === "DialogFieldTagControl" || dialogField.type === "DialogFieldDropDownList")
+            && dialogField.default_value instanceof Array) {
+          dialogFieldData['dialog_' + dialogField.name] = dialogField.default_value.join();
+        } else {
+          dialogFieldData['dialog_' + dialogField.name] = dialogField.default_value;
+        }
+      });
+
+      return dialogFieldData;
+    }
+    vm.saveRequest = saveRequest;
+
+    function submitSuccess(result) {
+      EventNotifications.success(result.message);
+      // $state.go('services.details', {serviceId: $stateParams.serviceId});
+    }
+
+    function submitFailure(result) {
+      EventNotifications.error(__('There was an error submitting this request: ') + result);
+    }
   }
 })();
