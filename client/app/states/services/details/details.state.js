f9ad2ee87cfa5f4e1a51cf801a9218254066d9c0
Chris Kacerguis <chriskacerguis@users.noreply.github.com>
2016-12-21 13:29:28 -0600

    Merge pull request #406 from syncrou/pull_real_power_status
    Pull power_status directly from the virtual column
    (cherry picked from commit 1f495cc700606eaba877face30bbc745aa808595)

--- client/app/states/services/details/details.state.js
+++ client/app/states/services/details/details.state.js
@@ -1,5 +1,5 @@
 (function() {
-  'use strict';
+'use strict';
 
   angular.module('app.states')
     .run(appRun);
@@ -43,6 +43,11 @@
       'service_template',
       'chargeback_report',
       'power_state',
+      'power_status',
+      'created_at',
+      'options',
+      'name',
+      'guid',
     ];
     var options = {
       attributes: requestAttributes,
