fa3060cb2600df5b29322ca734128f7d26e36227
Josh Langholtz <jjlangholtz@gmail.com>
2017-01-05 13:27:26 -0500

    Initial set-up for webpack-dev-server
    This change is the beginning of the migration from a gulp-based build
    system to a webpack-based one. Ideally, the existing gulp tasks should
    continue to work until all desired functionality is replicated to the
    webpack-based build for each environment (dev, test, prod). From this
    point forward, devs should use `yarn start` instead of `gulp serve` when
    starting the project locally.
    
    `yarn start` will now kickoff `webpack-dev-server` which should **mostly**
    work the same as the previous dev server. However, there is bound to be
    some issues that will be discovered/fixed over the coming weeks.

---
+++ client/app.js
@@ -0,0 +1,76 @@
+// The webpack entrypoint
+function requireAll(context) {
+  context.keys().forEach(context);
+}
+
+// Globals that are expected in application code and some libraries
+window.$ = window.jQuery = require('jquery');
+window._ = require('lodash');
+window.moment = require('moment');
+window.sprintf = require('sprintf-js');
+window.toastr = require('toastr');
+
+// Vendor libraries
+require('angular');
+require('angular-animate');
+require('angular-base64');
+require('angular-cookies');
+require('angular-drag-and-drop-lists');
+require('angular-dragdrop');
+require('angular-gettext');
+require('angular-messages');
+require('angular-mocks');
+require('angular-resource');
+require('angular-sanitize');
+require('angular-svg-base-fix');
+require('angular-ui-bootstrap');
+require('angular-ui-router');
+require('bootstrap');
+require('bootstrap-combobox');
+require('bootstrap-datepicker');
+require('bootstrap-select');
+require('bootstrap-switch');
+require('bootstrap-touchspin');
+require('c3');
+require('components-jqueryui');
+require('d3');
+require('datatables');
+require('datatables-colreorder');
+require('eonasdan-bootstrap-datetimepicker');
+require('es6-shim');
+require('google-code-prettify/bin/prettify.min.js');
+require('jquery-match-height');
+require('moment-timezone');
+require('ngprogress/build/ngProgress');
+require('ngstorage');
+require('patternfly-bootstrap-treeview/dist/bootstrap-treeview.min.js');
+require('patternfly/dist/js/patternfly.js');
+
+// Needs imports loader because it expects `this` to be `window`
+require('imports-loader?this=>window!actioncable');
+
+// Must be required last because of its dependencies
+require('angular-patternfly/dist/angular-patternfly');
+
+// Application files with strict ordering requirements
+require('./app/globals.js');
+require('./app/app.module.js');
+requireAll(require.context('.', true, /\.module\.js$/));
+requireAll(require.context('.', true, /^((?!module).)*\.js$/));
+
+// Vendor styles
+require('angular-patternfly/dist/styles/angular-patternfly.css');
+require('bootstrap-select/dist/css/bootstrap-select.css');
+require('bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.css');
+require('bootstrap-touchspin/src/jquery.bootstrap-touchspin.css');
+require('c3/c3.css');
+require('datatables/media/css/jquery.dataTables.css');
+require('eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css');
+require('google-code-prettify/bin/prettify.min.css');
+require('ngprogress/ngProgress.css');
+require('patternfly-bootstrap-treeview/dist/bootstrap-treeview.min.css');
+require('patternfly/dist/css/patternfly-additions.css');
+require('patternfly/dist/css/patternfly.css');
+
+// Application styles
+require('./assets/sass/styles.sass');
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const root = path.resolve(__dirname, './client');
+const dist = path.resolve(__dirname, './dist');
+
+module.exports = {
+  context: root,
+  entry: {
+    app: './app.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: dist,
+  },
+  devServer: {
+    contentBase: root,
+    historyApiFallback: true,
+    port: 3001,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+      },
+      '/pictures': {
+        target: 'http://localhost:3000',
+      },
+      '/ws': {
+        target: 'ws://localhost:3000',
+        ws: true,
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif|svg|woff|ttf|eot)/,
+        use: ['url-loader?limit=20480'],
+      },
+      {
+        test: /\.(css|s(a|c)ss)$/,
+
+        // 'loader' must be used instead of 'use' until fixed in `extract-text-webpack-plugin`
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: ['css-loader?importLoaders=2', 'postcss-loader', 'sass-loader'],
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new CopyWebpackPlugin([
+      { from: `${root}/assets` },
+    ]),
+  ],
+  performance: {
+    hints: false,
+  },
+};
