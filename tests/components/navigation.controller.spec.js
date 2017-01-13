98672ab7734cf14c3ec9ec085464159e0c82bb75
Dana Gutride <dgutride@redhat.com>
2017-01-13 11:46:06 -0500

    Add simple navigation controller tests to ensure sites value is set correctly

---
+++ tests/components/navigation.controller.spec.js
@@ -0,0 +1,53 @@
+describe('Navigation controller', function() {
+  beforeEach(module('app.components', function($provide) {
+    $provide.value('API_BASE', 'http://localhost');
+    $provide.value('gettextCatalog', function() {});
+  }));
+
+    describe('controller', function() {
+      var controller;
+      var ctrl;
+
+      beforeEach(inject(function($controller) {
+        bard.inject('Text',
+          'Navigation',
+          'Messages',
+          'Session',
+          'ShoppingCart',
+          'API_BASE',
+          '$rootScope',
+          '$uibModal',
+          '$state',
+          '$document',
+          'EventNotifications',
+          'ServerInfo',
+          'ProductInfo');
+
+      ctrl = $controller('NavigationController', {
+        Text: Text,
+        Navigation: Navigation,
+        Messages: Messages,
+        Session: Session,
+        API_BASE: API_BASE,
+        ShoppingCart: ShoppingCart,
+        $scope: $rootScope,
+        $uibModal: $uibModal,
+        $state: $state,
+        $document: $document,
+        EventNotifications: EventNotifications,
+        ServerInfo: ServerInfo,
+        ProductInfo: ProductInfo
+      });
+    }));
+
+    it('is defined', function() {
+      expect(ctrl).to.be.defined;
+    });
+
+    it('controller sites are defined and injected URL to be correct', function() {
+      expect(ctrl.sites).to.be.defined;
+      expect(ctrl.sites.length).to.equal(1);
+      expect(ctrl.sites[0].url).to.equal('http://localhost');
+    });
+  });
+});
