895e2fe0f5db110da9ca0e9dad6de8af8244038a
Allen Wight <awight@redhat.com>
2017-02-07 16:22:11 -0500

    Mocked additional ansible sections

--- client/app/components/services/service-details/service-details-ansible.component.js
+++ client/app/components/services/service-details/service-details-ansible.component.js
@@ -13,7 +13,47 @@ function ComponentController() {
   vm.$onInit = activate();
 
   function activate() {
+    angular.extend(vm, {
+      // Data
+      plays: {
+        open: true,
+        resources: [{
+          status: "successful",
+          name: "Sample Play",
+          description: "Sample play description",
+          started: new Date(),
+          run_time: 6000
+        }],
+      },
+      creds: {
+        open: true,
+        resources: [{provider: "cloud", credential: "this is a sample credential"}]
+      },
+      output: "",
 
+      // Functions
+      sampleAction: angular.noop(),
+      viewPlay: angular.noop(),
+
+      // Config
+      credListConfig: credListConfig(),
+      playsListConfig: playsListConfig(),
+    });
   }
 
+  function credListConfig() {
+    return {
+      showSelectBox: false,
+      selectionMatchProp: 'id',
+    }
+  }
+
+  function playsListConfig() {
+    return {
+      showSelectBox: false,
+      selectionMatchProp: 'id',
+    };
+  }
 }
+
+
