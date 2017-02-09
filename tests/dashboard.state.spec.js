48e2b96f84cbf8392ec371d5510dd62293800dcf
Chris Hale <chris.hale@me.com>
2017-02-09 11:32:20 -0700

    Fixed dashboard test failure

--- tests/dashboard.state.spec.js
+++ tests/dashboard.state.spec.js
@@ -1,5 +1,6 @@
 describe('Dashboard', function() {
   beforeEach(function() {
+    module('app.core');
     module('app.states', bard.fakeToastr);
     bard.inject('$location', '$rootScope', '$state', '$templateCache', 'Session', '$httpBackend', '$q', 'RBAC');
   });
@@ -42,7 +43,7 @@ describe('Dashboard', function() {
     var resolveAllRequests = [];
 
     beforeEach(function() {
-      bard.inject('$controller', '$log', '$state', '$rootScope', 'CollectionsApi','RBAC');
+      bard.inject('$controller', '$log', '$state', '$rootScope', 'CollectionsApi', 'RBAC');
 
       var controllerResolves = {
         definedServiceIdsServices: resolveServicesWithDefinedServiceIds,
@@ -68,7 +69,9 @@ describe('Dashboard', function() {
       it('makes a query request using the CollectionApi', function() {
         var clock = sinon.useFakeTimers(new Date('2016-01-01').getTime());
         let collectionsApiSpy = sinon.stub(CollectionsApi);
-        dashboardState.resolve.expiringServices(collectionsApiSpy);
+        RBAC.setNavFeatures({ "dashboard": { "show": true }, "services": { "show": true }, "orders": { "show": true }, "requests": { "show": true }, "catalogs": { "show": true }, "dialogs": { "show": true }, "administration": { "show": true } });
+
+        dashboardState.resolve.expiringServices(collectionsApiSpy, RBAC);
 
         expect(collectionsApiSpy.query).to.have.been.calledWith('services', {
           hide: 'resources',
