219ef4de8044bedc8b25e08fab4b60402ad0175f
Chris Hale <chris.hale@me.com>
2017-02-16 13:43:26 -0700

    Updates to list format based on design feedback

--- client/app/core/templates.service.js
+++ client/app/core/templates.service.js
@@ -42,9 +42,30 @@ export function TemplatesFactory(CollectionsApi) {
   function getQueryFilters(filters) {
     const queryFilters = [];
 
-    angular.forEach(filters, function(nextFilter) {
+    angular.forEach(filters, function (nextFilter) {
       if (nextFilter.id === 'name') {
         queryFilters.push("name='%" + nextFilter.value + "%'");
+      } else if (nextFilter.id === 'type') {
+        var filterText = nextFilter.value;
+        var types = [
+          { "expression": "aws*|amazon*|cloud\s?(formation)?", "type": "OrchestrationTemplateCfn" },
+          { "expression": "open\s?(stack)?", "type": "OrchestrationTemplateHot" },
+          { "expression": "vmware|vapps|vcenter", "type": "OrchestrationTemplateVapp" },
+          { "expression": "azure|microsoft", "type": "OrchestrationTemplateAzure" },
+        ];
+        for (var i = 0; i < types.length; i++) {
+          var templateType = types[i];
+          var regex = new RegExp(templateType.expression, "i");
+          if (filterText.match(regex)) {
+            if (templateType.type === 'OrchestrationTemplateHot') {
+              queryFilters.push(nextFilter.id + '=' + templateType.type);
+              queryFilters.push('or ' + nextFilter.id + '=OrchestrationTemplateVnfd');
+            } else {
+              queryFilters.push(nextFilter.id + '=' + templateType.type);
+            }
+            break;
+          }
+        }
       } else {
         queryFilters.push(nextFilter.id + '=' + nextFilter.value);
       }
