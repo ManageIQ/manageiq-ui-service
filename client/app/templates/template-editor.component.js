db3dddbd74c40e1bde09593b1a13c13c22992a7a
Chris Hale <chris.hale@me.com>
2017-02-28 10:46:32 -0700

    Changed to constant

--- client/app/templates/template-editor.component.js
+++ client/app/templates/template-editor.component.js
@@ -150,7 +150,7 @@ function ComponentController($state, sprintf, TemplatesService, EventNotificatio
     $state.go('templates.editor', { templateId: vm.template.id });
   }
   function downloadTemplate() {
-    let data = new Blob([vm.template.content], { type: 'text/plain;charset=utf-8' });
+    const data = new Blob([vm.template.content], { type: 'text/plain;charset=utf-8' });
     FileSaver.saveAs(data, 'Template.txt');
   }
   function removeTemplate() {
