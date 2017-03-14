0b2794e9eb5d8c0d8b5eea40354a74a84099f775
Chris Hale <chris.hale@me.com>
2017-03-14 08:36:34 -0600

    Revised permission listing

--- client/app/requests/requests-state.service.js
+++ client/app/requests/requests-state.service.js
@@ -8,8 +8,8 @@ export function RequestsStateFactory(ListConfiguration, RBAC) {
 
   function getPermissions() {
     const permissions = {
-      approval: RBAC.hasAny(['miq_request_approval','miq_request_admin','miq_request','miq_request_control']),
-      edit: RBAC.hasAny(['miq_request_edit','miq_request_admin','miq_request','miq_request_control']),
+      approval: RBAC.hasAny(['miq_request_approval']),
+      edit: RBAC.hasAny(['miq_request_edit']),
     };
 
     return permissions;
