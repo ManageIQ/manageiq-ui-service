801673ecc24a449408c27630491cdf7c71cce090
Chris Kacerguis <chriskacerguis@users.noreply.github.com>
2017-03-29 13:47:17 -0500

    Merge pull request #613 from chalettu/report-details
    Created Report details component and pages
    (cherry picked from commit 5f7912d2ae0bd94dcb95f2b1e77f54b345ce7d55)

---
+++ client/app/reports/reports-details.component.spec.js
@@ -0,0 +1,32 @@
+describe('Component: reportsDetails', function() {
+
+  beforeEach(function() {
+    module('app.states','app.core', 'app.reports');
+  });
+
+  describe('with $compile', function() {
+    let scope;
+    let element;
+    let getReportSpy;
+    let mockDir = 'tests/mock/reports/';
+
+    beforeEach(inject(function($compile, $rootScope) {
+      bard.inject('ReportsService', '$state', '$httpBackend');
+      scope = $rootScope.$new();
+      scope.report = readJSON(mockDir + 'report.json');
+      element = angular.element('<reports-details report="report"/>');
+      $compile(element)(scope);
+      $httpBackend.whenGET('').respond(200);
+      scope.reportRuns = readJSON(mockDir + 'report.details.json');
+      getReportSpy = sinon.stub(ReportsService, 'getReportRuns').returns(Promise.resolve(scope.reportRuns));
+
+      scope.$apply();
+    }));
+
+    it('should work with $state.go', function() {
+      $state.go('reports.details',{reportId:'10000000000375'});
+      expect($state.is('reports.details'));
+    });
+  });
+
+});
