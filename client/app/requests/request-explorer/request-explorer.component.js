bef5df0ce26ba17ca2bff4e2ad1c61495da29e9c
Chris Kacerguis <chriskacerguis@users.noreply.github.com>
2017-03-31 13:00:20 -0500

    Merge pull request #621 from AllenBW/enhancement/140565959-update-requests-explorer
    PT#140565959-Requestes sorted by most recent
    (cherry picked from commit 3a97d9d01c9d87bb53740a1530ffed07e42a4437)

--- client/app/requests/request-explorer/request-explorer.component.js
+++ client/app/requests/request-explorer/request-explorer.component.js
@@ -2,31 +2,30 @@ import '../../../assets/sass/_explorer.sass';
 import templateUrl from './request-explorer.html';
 
 export const RequestExplorerComponent = {
-  templateUrl,
   controller: ComponentController,
   controllerAs: 'vm',
+  templateUrl,
 };
 
 /** @ngInject */
-function ComponentController($state, CollectionsApi, RequestsState, ListView, $filter, lodash, Language, EventNotifications,
+function ComponentController($state, RequestsState, ListView, lodash, Language, EventNotifications,
                              ModalService, Polling) {
-  var vm = this;
-  vm.permissions = RequestsState.getPermissions();
+  const vm = this;
+
+  vm.$onDestroy = function() {
+    Polling.stop('requestsListPolling');
+  };
+
   vm.$onInit = function() {
-    if ($state.params.filter) {
-      RequestsState.setFilters($state.params.filter);
-      RequestsState.filterApplied = true;
-    } else {
-      RequestsState.setFilters([]);
-      RequestsState.filterApplied = false;
-    }
+    vm.permissions = RequestsState.getPermissions();
+    RequestsState.listActions.setSort(RequestsState.listActions.getSort().currentField, false);
+    $state.params.filter ? RequestsState.listActions.setFilters($state.params.filter) : RequestsState.listActions.setFilters([]);
 
     angular.extend(vm, {
       title: __('Request Explorer'),
       listData: [],
-      listDataCopy: [],
       loading: true,
-      requestCount: 0,
+      filterCount: 0,
       limit: 20,
       offset: 0,
       selectedItemsList: [],
@@ -42,15 +41,15 @@ function ComponentController($state, CollectionsApi, RequestsState, ListView, $f
         },
         filterConfig: {
           fields: getRequestFilterFields(),
-          resultsCount: vm.listDataCopy ? vm.listDataCopy.length : 0,
-          appliedFilters: RequestsState.filterApplied ? RequestsState.getFilters() : [],
+          resultsCount: 0,
+          appliedFilters: RequestsState.listActions.getFilters(),
           onFilterChange: filterChange,
         },
         sortConfig: {
           fields: getRequestSortFields(),
           onSortChange: sortChange,
-          isAscending: RequestsState.getSort().isAscending,
-          currentField: RequestsState.getSort().currentField,
+          isAscending: RequestsState.listActions.getSort().isAscending,
+          currentField: RequestsState.listActions.getSort().currentField,
         },
       },
       footerConfig: {
@@ -91,91 +90,39 @@ function ComponentController($state, CollectionsApi, RequestsState, ListView, $f
         },
       ];
     }
-    vm.fetchData = fetchData;
-    vm.fetchData(vm.limit, 0);
 
-    if (RequestsState.filterApplied) {
-      /* Apply the filtering to the data list */
-      filterChange(RequestsState.getFilters());
-      RequestsState.filterApplied = false;
-    } else {
-      applyFilters();
-    }
+    fetchData(vm.limit, 0);
 
-    Language.fixState(RequestsState, vm.toolbarConfig);
+    Language.fixState(RequestsState.listActions, vm.toolbarConfig);
     Polling.start('requestsListPolling', pollUpdateRequestsList, vm.pollingInterval);
-    vm.$onDestroy = function() {
-      Polling.stop('requestsListPolling');
-    };
   };
 
   // Private
 
-  function fetchData(limit, offset, _refresh) {
-    var filter = [];
-    var attributes = [
-      'approval_state',
-      'created_on',
-      'description',
-      'message',
-      'picture',
-      'picture.image_href',
-      'requester_name',
-    ];
-    var options = {
-      expand: 'resources',
-      limit: limit,
-      offset: String(offset),
-      attributes: attributes,
-      filter: filter,
-    };
+  function fetchData(limit, offset, refresh) {
+    vm.loading = !refresh;
 
-    if (vm.toolbarConfig.filterConfig.appliedFilters) {
-      lodash.forEach(lodash.groupBy(vm.toolbarConfig.filterConfig.appliedFilters, 'id'), processFilters);
-    }
-
-    CollectionsApi.query('requests', {hide: 'resources', filter: filter}).then(handleSuccess, handleError);
-    CollectionsApi.query('requests', options).then(handleSuccess, handleError);
+    RequestsState.get(limit, offset).then(handleSuccess, handleError);
 
     function handleSuccess(response) {
-      if (angular.isDefined(response.resources)) {
-        var responseData = response.resources; // vm.selectedItemsList
-        if (vm.selectedItemsList.length > 0) {
-          responseData.forEach(function(item) {
-            var selectedItem = lodash.find(vm.selectedItemsList, {id: item.id});
-            if (angular.isDefined(selectedItem)) {
-              item.selected = true;
-            }
-          });
-        }
-        vm.listData = responseData;
-        vm.listDataCopy = angular.copy(vm.listData);
-        vm.loading = false;
-      } else {
-        vm.requestCount = response.subcount;
-        vm.toolbarConfig.filterConfig.resultsCount = vm.requestCount;
-      }
-      sortChange(RequestsState.getSort().currentField, RequestsState.getSort().isAscending);
+      vm.loading = false;
+      vm.selectedItemsList = [];
+      vm.listData = response.resources;
+
+      RequestsState.getMinimal().then((response) => {
+        vm.toolbarConfig.filterConfig.resultsCount = response.subcount;
+        vm.filterCount = response.subcount;
+      });
     }
 
     function handleError() {
       vm.loading = false;
       EventNotifications.error(__('There was an error loading the requests.'));
     }
-
-    function processFilters(value) {
-      var count = 0;
-      lodash.forEach(value, pushFilterValue);
-
-      function pushFilterValue(item) {
-        count >= 1 ? options.filter.push('or ' + item.id + '=' + item.value) : options.filter.push(item.id + '=' + item.value);
-        count++;
-      }
-    }
   }
 
   function getRequestFilterFields() {
-    var statuses = [__('pending_approval'), __('denied'), __('approved')];
+    const statuses = [__('pending_approval'), __('denied'), __('approved')];
 
     return [
       ListView.createFilterField('description', __('Description'), __('Filter by Description'), 'text'),
@@ -191,7 +138,7 @@ function ComponentController($state, CollectionsApi, RequestsState, ListView, $f
       ListView.createSortField('description', __('Description'), 'alpha'),
       ListView.createSortField('id', __('Request ID'), 'numeric'),
       ListView.createSortField('requester_name', __('Requester'), 'alpha'),
-      ListView.createSortField('requested', __('Request Date'), 'numeric'),
+      ListView.createSortField('created_on', __('Request Date'), 'numeric'),
       ListView.createSortField('status', __('Request Status'), 'alpha'),
     ];
   }
@@ -200,71 +147,14 @@ function ComponentController($state, CollectionsApi, RequestsState, ListView, $f
     $state.go('requests.details', {requestId: item.id});
   }
 
-  function sortChange(sortId, _direction) {
-    vm.listDataCopy.sort(compareFn);
-
-    /* Keep track of the current sorting state */
-    RequestsState.setSort(sortId, vm.toolbarConfig.sortConfig.isAscending);
-  }
-
-  function compareFn(item1, item2) {
-    var compValue = 0;
-    if (vm.toolbarConfig.sortConfig.currentField.id === 'description') {
-      compValue = item1.description.localeCompare(item2.description);
-    } else if (vm.toolbarConfig.sortConfig.currentField.id === 'id') {
-      compValue = item1.id - item2.id;
-    } else if (vm.toolbarConfig.sortConfig.currentField.id === 'requester_name') {
-      compValue = item1.requester_name.localeCompare(item2.requester_name);
-    } else if (vm.toolbarConfig.sortConfig.currentField.id === 'requested') {
-      compValue = new Date(item1.created_on) - new Date(item2.created_on);
-    } else if (vm.toolbarConfig.sortConfig.currentField.id === 'status') {
-      compValue = item1.approval_state.localeCompare(item2.approval_state);
-    }
-
-    if (!vm.toolbarConfig.sortConfig.isAscending) {
-      compValue = compValue * -1;
-    }
-
-    return compValue;
+  function sortChange(sortId, direction) {
+    RequestsState.listActions.setSort(sortId, direction);
+    fetchData(vm.limit, 0);
   }
 
   function filterChange(filters) {
-    applyFilters(filters);
-    vm.toolbarConfig.filterConfig.resultsCount = vm.listDataCopy.length;
-  }
-
-  function applyFilters(filters) {
-    vm.selectedItemsList = [];
-    vm.listDataCopy = ListView.applyFilters(filters, vm.listDataCopy, vm.listData, RequestsState, requestMatchesFilter);
-    vm.fetchData(vm.limit, 0);
-
-    /* Make sure sorting direction is maintained */
-    sortChange(RequestsState.getSort().currentField, RequestsState.getSort().isAscending);
-  }
-
-  function requestMatchesFilter(item, filter) {
-    if (filter.id === 'description') {
-      return item.description.toLowerCase().indexOf(filter.value.toLowerCase()) !== -1;
-    } else if (filter.id === 'approval_state') {
-      var value;
-      if (lodash.lastIndexOf([__('Pending'), 'Pending'], filter.value) > -1) {
-        value = "pending_approval";
-      } else if (lodash.lastIndexOf([__('Denied'), 'Denied'], filter.value) > -1) {
-        value = "denied";
-      } else if (lodash.lastIndexOf([__('Approved'), 'Approved'], filter.value) > -1) {
-        value = "approved";
-      }
-
-      return item.approval_state === value;
-    } else if (filter.id === 'request_id') {
-      return String(item.id).toLowerCase().indexOf(filter.value.toLowerCase()) !== -1;
-    } else if (filter.id === 'requester_name') {
-      return String(item.requester_name).toLowerCase().indexOf(filter.value.toLowerCase()) !== -1;
-    } else if (filter.id === 'created_on') {
-      return $filter('date')(item.created_on).toLowerCase().indexOf(filter.value.toLowerCase()) !== -1;
-    }
-
-    return false;
+    RequestsState.listActions.setFilters(filters);
+    fetchData(vm.limit, 0);
   }
 
   function selectItem(item) {
@@ -273,7 +163,7 @@ function ComponentController($state, CollectionsApi, RequestsState, ListView, $f
   }
 
   function approveRequests() {
-    var modalOptions = {
+    const modalOptions = {
       component: 'processRequestsModal',
       resolve: {
         requests: function() {
@@ -288,7 +178,7 @@ function ComponentController($state, CollectionsApi, RequestsState, ListView, $f
   }
 
   function denyRequests() {
-    var modalOptions = {
+    const modalOptions = {
       component: 'processRequestsModal',
       resolve: {
         requests: function() {
@@ -311,12 +201,12 @@ function ComponentController($state, CollectionsApi, RequestsState, ListView, $f
   }
 
   function pollUpdateRequestsList() {
-    vm.fetchData(vm.limit, vm.offset, true);
+    fetchData(vm.limit, vm.offset, true);
   }
 
   function paginationUpdate(limit, offset) {
     vm.limit = limit;
     vm.offset = offset;
-    vm.fetchData(limit, offset);
+    fetchData(limit, offset);
   }
 }
