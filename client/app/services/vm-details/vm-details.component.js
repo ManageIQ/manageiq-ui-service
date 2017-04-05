cc2e1327afb5765396a6c0ee01978f13ee9355e1
Allen Wight <awight@redhat.com>
2017-04-05 15:40:15 -0400

    Add power operatios to vmDetails component
    - obeys rbac
    - obeys power state
    - observes polling at same rate as serviceDetails

--- client/app/services/vm-details/vm-details.component.js
+++ client/app/services/vm-details/vm-details.component.js
@@ -3,69 +3,77 @@ import './_vm-details.sass';
 import templateUrl from './vm-details.html';
 
 export const VmDetailsComponent = {
-  templateUrl,
   controller: ComponentController,
   controllerAs: 'vm',
-  bindToController: true,
+  templateUrl,
 };
 
 /** @ngInject */
-function ComponentController($stateParams, VmsService, sprintf, EventNotifications) {
+function ComponentController($stateParams, VmsService, ServicesState, sprintf, EventNotifications, Polling, PowerOperations) {
   const vm = this;
+  vm.$onInit = activate;
+  vm.$onDestroy = onDestroy;
 
-  vm.$onInit = function() {
-    vm.permissions = VmsService.getPermissions();
+  function onDestroy() {
+    Polling.stop('polling');
+  }
 
+  function activate() {
+    vm.permissions = ServicesState.getPermissions();
     angular.extend(vm, {
+      loading: true,
+      neverText: __('Never'),
+      noneText: __('None'),
+      availableText: __('Available'),
+      notAvailable: __("Not Available"),
       vmDetails: {},
       instance: {},
-      loading: false,
+      // Config
+      headerConfig: {
+        actionsConfig: {
+          actionsInclude: true,
+        },
+      },
     });
-
     resolveData();
-  };
+    Polling.start('polling', resolveData, 50000);
+  }
 
+// Private
+  function startVM() {
+    PowerOperations.startVm(vm.vmDetails);
+  }
 
-  // Private
-  function processInstanceVariables(data) {
-    const noneText = __('None');
-    data.availabilityZone = (angular.isUndefined(data.availability_zone) ? noneText : data.availability_zone.name);
-    data.cloudTenant = (angular.isUndefined(data.cloud_tenant) ? noneText : data.cloud_tenant);
-    data.orchestrationStack = ( angular.isUndefined(data.orchestration_stack) ? noneText : data.orchestration_stack);
-    data.keyPairLabels = [];
-    data.key_pairs.forEach(function(keyPair) {
-      data.keyPairLabels.push(keyPair.name);
-    });
+  function stopVM() {
+    PowerOperations.stopVm(vm.vmDetails);
+  }
 
-    vm.vmDetails.instance = data;
+  function suspendVM() {
+    PowerOperations.suspendVm(vm.vmDetails);
+  }
+
+  function retireVM() {
+    PowerOperations.retireVM(vm.vmDetails);
   }
 
   function resolveData() {
     VmsService.getVm($stateParams.vmId).then(handleSuccess, handleFailure);
 
     function handleSuccess(response) {
-      const neverText = __('Never');
-      const noneText = __('None');
-      const availableText = __('Available');
-      const notAvailable = __("Not Available");
-
-
       if (response.cloud) {
         VmsService.getInstance(response.id).then((response) => {
           vm.instance = response;
         });
       }
-
       vm.vmDetails = response;
-
-      vm.vmDetails.lastSyncOn = (angular.isUndefined(vm.vmDetails.last_sync_on) ? neverText : vm.vmDetails.last_sync_on);
-      vm.vmDetails.retiresOn = (angular.isUndefined(vm.vmDetails.retires_on) ? neverText : vm.vmDetails.retires_on);
+      vm.vmDetails.lastSyncOn = (angular.isUndefined(vm.vmDetails.last_sync_on) ? vm.neverText : vm.vmDetails.last_sync_on);
+      vm.vmDetails.retiresOn = (angular.isUndefined(vm.vmDetails.retires_on) ? vm.neverText : vm.vmDetails.retires_on);
       vm.vmDetails.snapshotCount = defaultText(vm.vmDetails.snapshots);
-      vm.vmDetails.resourceAvailability = (vm.vmDetails.template === false ? availableText : noneText);
+      vm.vmDetails.resourceAvailability = (vm.vmDetails.template === false ? vm.availableText : vm.noneText);
       vm.vmDetails.driftHistory = defaultText(vm.vmDetails.drift_states);
       vm.vmDetails.scanHistoryCount = defaultText(vm.vmDetails.scan_histories);
       vm.vmDetails.lastComplianceStatus = (angular.isUndefined(vm.vmDetails.last_compliance_status) ? __('Never Verified') : vm.vmDetails.last_compliance_status);
-      vm.vmDetails.complianceHistory = (vm.vmDetails.compliances.length > 0 ? availableText : notAvailable);
+      vm.vmDetails.complianceHistory = (vm.vmDetails.compliances.length > 0 ? vm.availableText : vm.notAvailable);
       vm.vmDetails.provisionDate = angular.isDefined(vm.vmDetails.service.miq_request) ? vm.vmDetails.service.miq_request.fulfilled_on : __('Unknown');
       vm.vmDetails.containerSpecsText = vm.vmDetails.vendor + ': ' + vm.vmDetails.hardware.cpu_total_cores + ' CPUs (' + vm.vmDetails.hardware.cpu_sockets
         + ' sockets x ' + vm.vmDetails.hardware.cpu_cores_per_socket + ' core), ' + vm.vmDetails.hardware.memory_mb + ' MB';
@@ -76,12 +84,66 @@ function ComponentController($stateParams, VmsService, sprintf, EventNotificatio
       if (vm.vmDetails.retired) {
         EventNotifications.warn(sprintf(__("%s is a retired resource"), vm.vmDetails.name), {persistent: true, unread: false});
       }
+      getListActions();
+      vm.loading = false;
     }
 
     function handleFailure(_error) {
       EventNotifications.error(__('There was an error loading the vm details.'));
     }
   }
+
+  function getListActions() {
+    vm.listActions = [];
+    const powerOptionsMenu = {
+      title: __('Power Operations'),
+      actionName: 'powerOperations',
+      icon: 'fa fa-power-off',
+      actions: [],
+      isDisabled: false,
+    };
+    const powerOptionsActions = [
+      {
+        icon: 'fa fa-play',
+        name: __('Start'),
+        actionName: 'start',
+        title: __('Start the Service'),
+        actionFn: startVM,
+        permission: vm.permissions.instanceStart,
+        isDisabled: vm.vmDetails.power_state === 'on',
+      }, {
+        icon: 'fa fa-stop',
+        name: __('Stop'),
+        actionName: 'stop',
+        title: __('Stop the Service'),
+        actionFn: stopVM,
+        permission: vm.permissions.instanceStop,
+        isDisabled: vm.vmDetails.power_state !== 'on',
+      }, {
+        icon: 'fa fa-pause',
+        name: __('Suspend'),
+        actionName: 'suspend',
+        title: __('Suspend the Service'),
+        actionFn: suspendVM,
+        permission: vm.permissions.instanceSuspend,
+        isDisabled: vm.vmDetails.power_state !== 'on',
+      }, {
+        icon: 'fa fa-bed',
+        name: __('Retire'),
+        actionName: 'retire',
+        title: __('Retire the Service'),
+        actionFn: retireVM,
+        permission: vm.permissions.instanceRetire,
+        isDisabled: vm.vmDetails.power_state !== 'on',
+
+      },
+    ];
+    powerOptionsActions.forEach((menuOption) => {
+      menuOption.permission ? powerOptionsMenu.actions.push(menuOption) : false;
+    });
+    powerOptionsMenu.actions.length ? vm.listActions.push(powerOptionsMenu) : false;
+  }
+
 
   function defaultText(inputCount, defaultText) {
     const inputArrSize = inputCount.length;
@@ -91,5 +153,18 @@ function ComponentController($stateParams, VmsService, sprintf, EventNotificatio
     } else {
       return inputArrSize;
     }
+  }
+
+
+  function processInstanceVariables(data) {
+    data.availabilityZone = (angular.isUndefined(data.availability_zone) ? vm.noneText : data.availability_zone.name);
+    data.cloudTenant = (angular.isUndefined(data.cloud_tenant) ? vm.noneText : data.cloud_tenant);
+    data.orchestrationStack = ( angular.isUndefined(data.orchestration_stack) ? vm.noneText : data.orchestration_stack);
+    data.keyPairLabels = [];
+    data.key_pairs.forEach(function(keyPair) {
+      data.keyPairLabels.push(keyPair.name);
+    });
+
+    vm.vmDetails.instance = data;
   }
 }
