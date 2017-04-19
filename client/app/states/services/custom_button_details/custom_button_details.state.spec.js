1e30d397c40ad1070cdc76430f0d6d787bfdc55f
Chris Kacerguis <chriskacerguis@users.noreply.github.com>
2017-04-19 13:42:42 -0500

    Merge pull request #692 from chalettu/dynamic-dropdown-defaults
    BZ 1442383 - Self Service UI does not properly select defaults for dynamic drop downs
    (cherry picked from commit 17895f25cd8a4acef3bec05376100c76d075657c)

--- client/app/states/services/custom_button_details/custom_button_details.state.spec.js
+++ client/app/states/services/custom_button_details/custom_button_details.state.spec.js
@@ -64,7 +64,7 @@ describe('services.custom_button_details', function() {
             'services',
             123,
             {},
-            '{"action":"buttonName","resource":{"dialogField1":"1","dialogField2":"2"}}'
+            '{"action":"buttonName","resource":{"dialogField1":1,"dialogField2":2}}'
           );
         });
 
