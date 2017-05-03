8b2b2929b05464c9bcaef5622123280305d86ef8
Chris Kacerguis <chriskacerguis@users.noreply.github.com>
2017-05-03 17:44:16 -0500

    Merge pull request #738 from chalettu/dashboard-orders
    Dashboard is missing information on Orders
    (cherry picked from commit da44d75d00e44e8dac72954ce80c64f72d26847e)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1449792

--- client/app/states/dashboard/dashboard.state.js
+++ client/app/states/dashboard/dashboard.state.js
@@ -21,9 +21,75 @@ function getStates() {
         definedServiceIdsServices: resolveServicesWithDefinedServiceIds,
         retiredServices: resolveRetiredServices,
         expiringServices: resolveExpiringServices,
+        allRequests: resolveAllRequests,
       },
     },
   };
+}
+
+/** @ngInject */
+
+function resolveAllRequests(CollectionsApi, RBAC) {
+  if (!RBAC.has('miq_request_view')) {
+    return undefined;
+  }
+
+  return [
+    [
+      pendingRequestsForServiceTemplateProvisionRequest(CollectionsApi),
+      pendingRequestsForServiceReconfigureRequest(CollectionsApi),
+    ],
+    [
+      approvedRequestsForServiceTemplateProvisionRequest(CollectionsApi),
+      approvedRequestsForServiceReconfigureRequest(CollectionsApi),
+    ],
+    [
+      deniedRequestsForServiceTemplateProvisionRequest(CollectionsApi),
+      deniedRequestsForServiceReconfigureRequest(CollectionsApi),
+    ],
+  ];
+}
+
+function pendingRequestsForServiceTemplateProvisionRequest(CollectionsApi) {
+  var filterValues = ['approval_state=pending_approval'];
+  var options = {hide: 'resources', filter: filterValues};
+
+  return CollectionsApi.query('requests', options);
+}
+
+function pendingRequestsForServiceReconfigureRequest(CollectionsApi) {
+  var filterValues = ['type=ServiceReconfigureRequest', 'approval_state=pending_approval'];
+  var options = {hide: 'resources', filter: filterValues};
+
+  return CollectionsApi.query('requests', options);
+}
+
+function approvedRequestsForServiceTemplateProvisionRequest(CollectionsApi) {
+  var filterValues = ['type=ServiceTemplateProvisionRequest', 'approval_state=approved'];
+  var options = {hide: 'resources', filter: filterValues};
+
+  return CollectionsApi.query('requests', options);
+}
+
+function approvedRequestsForServiceReconfigureRequest(CollectionsApi) {
+  var filterValues = ['type=ServiceReconfigureRequest', 'approval_state=approved'];
+  var options = {hide: 'resources', filter: filterValues};
+
+  return CollectionsApi.query('requests', options);
+}
+
+function deniedRequestsForServiceTemplateProvisionRequest(CollectionsApi) {
+  var filterValues = ['type=ServiceTemplateProvisionRequest', 'approval_state=denied'];
+  var options = {hide: 'resources', filter: filterValues};
+
+  return CollectionsApi.query('requests', options);
+}
+
+function deniedRequestsForServiceReconfigureRequest(CollectionsApi) {
+  var filterValues = ['type=ServiceReconfigureRequest', 'approval_state=denied'];
+  var options = {hide: 'resources', filter: filterValues};
+
+  return CollectionsApi.query('requests', options);
 }
 
 /** @ngInject */
@@ -69,7 +135,7 @@ function resolveServicesWithDefinedServiceIds(CollectionsApi, RBAC) {
 }
 
 /** @ngInject */
-function StateController($state, definedServiceIdsServices, retiredServices, expiringServices, lodash, Chargeback) {
+function StateController($state, definedServiceIdsServices, retiredServices, expiringServices, allRequests, lodash, $q, Chargeback) {
   var vm = this;
 
   if (angular.isDefined(definedServiceIdsServices)) {
@@ -97,6 +163,19 @@ function StateController($state, definedServiceIdsServices, retiredServices, exp
     vm.servicesFeature = true;
   }
 
+  vm.requestsFeature = false;
+  if (angular.isDefined(allRequests)) {
+    vm.requestsCount = {};
+    vm.requestsCount.total = 0;
+
+    var allRequestTypes = ['pending', 'approved', 'denied'];
+    allRequests.forEach(function(promise, n) {
+      resolveRequestPromises(promise, allRequestTypes[n], lodash, $q);
+    });
+
+    vm.requestsFeature = true;
+  }
+
   vm.navigateToServicesList = function(filterValue) {
     $state.go('services', {'filter': [{'id': 'retirement', 'title': __('Retirement Date'), 'value': filterValue}]});
   };
@@ -105,14 +184,14 @@ function StateController($state, definedServiceIdsServices, retiredServices, exp
     $state.go('services', {'filter': [{'id': 'retired', 'title': __('Retired'), 'value': true}]});
   };
 
-  vm.navigateToRetiringSoonServicesList = function() {
+  vm.navigateToRetiringSoonServicesList = function () {
     var currentDate = new Date();
     var filters = [];
 
-    filters.push({'id': 'retires_on', 'operator': '>', 'value': currentDate.toISOString()});
-    filters.push({'id': 'retired', 'title': __('Retired'), 'value': false});
+    filters.push({ 'id': 'retires_on', 'operator': '>', 'value': currentDate.toISOString() });
+    filters.push({ 'id': 'retired', 'title': __('Retired'), 'value': false });
     var days30 = currentDate.setDate(currentDate.getDate() + 30);
-    filters.push({'id': 'retires_on', 'operator': '<', 'value': new Date(days30).toISOString()});
+    filters.push({ 'id': 'retires_on', 'operator': '<', 'value': new Date(days30).toISOString() });
 
     $state.go('services', {'filter': filters});
   };
@@ -120,4 +199,12 @@ function StateController($state, definedServiceIdsServices, retiredServices, exp
   vm.navigateToCurrentServicesList = function() {
     $state.go('services', {'filter': [{'id': 'retired', 'title': 'Retired', 'value': false}]});
   };
+
+  function resolveRequestPromises(promiseArray, type, lodash, $q) {
+    $q.all(promiseArray).then(function(data) {
+      var count = lodash.sum(data, 'subcount');
+      vm.requestsCount[type] = count;
+      vm.requestsCount.total += count;
+    });
+  }
 }
