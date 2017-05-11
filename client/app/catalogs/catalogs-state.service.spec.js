b99f37f76b87ba0cc14937fc92092aab3b0e5625
Chris Hale <chris.hale@me.com>
2017-05-11 14:20:33 -0600

    Improved test coverage for catalog state service

--- client/app/catalogs/catalogs-state.service.spec.js
+++ client/app/catalogs/catalogs-state.service.spec.js
@@ -20,24 +20,94 @@ describe('CatalogsState', function() {
 
     it('should query the API for catalogs', function() {
       collectionsApiSpy = sinon.stub(CollectionsApi, 'query').returns(Promise.resolve(successResponse));
-
-      CatalogsState.getCatalogs(5, 0).then(function(response) {
+      CatalogsState.getCatalogs();
         expect(collectionsApiSpy).to.have.been.calledWith(
           'service_catalogs',
           {
-            expand: ['resources', 'service_templates'],
-            attributes: ['tenant', 'picture', 'picture.image_href', 'service_template_catalog.name', 'dialogs'],
+           expand: ['resources']
+          }
+        );
+    });
+    it('should support sorting',() => {
+      CatalogsState.setSort('name', true);
+      const sort = CatalogsState.getSort();
+      const expectedSort = {
+        isAscending: true,
+        currentField: 'name'
+      };
+      expect(sort).to.eql(expectedSort);
+    });
+     it('should support setting filters',() => {
+      const filters =[
+        {'id':'name', 'value':'test'}
+      ];
+      CatalogsState.setFilters(filters);
+      const filterCount = CatalogsState.getFilters();
+      expect(filterCount.length).to.eql(1);
+    });
+    it('should be able query service templates', () => {
+       collectionsApiSpy = sinon.stub(CollectionsApi, 'query').returns(Promise.resolve(successResponse));
+     /*  const filters =[
+        {'id':'name', 'value':'test'}
+      ];
+      CatalogsState.setFilters(filters);*/
+      CatalogsState.getServiceTemplates(5,0);
+        expect(collectionsApiSpy).to.have.been.calledWith(
+          'service_templates',
+          {
+            expand: ['resources'],
+            attributes: ['picture', 'picture.image_href', 'service_template_catalog'],
             limit: 5,
             offset: 0,
-            filter: [],
+            filter: ['display=true'],
             sort_by: 'name',
             sort_order: 'asc',
             sort_options: 'ignore_case'
           }
         );
-
-      });
     });
-  });
+    it('should be able to get permissions', () =>{
+      const permissions=CatalogsState.getPermissions();
+      const expectedPermissions = {
+        create: false,
+        edit: false,
+        delete: false
+      };
+      expect(permissions).to.eql(expectedPermissions);
+    });
+
+    it('should be able to get record Counts ', () => {
+      collectionsApiSpy = sinon.stub(CollectionsApi, 'query').returns(Promise.resolve(successResponse));
+      CatalogsState.getMinimal('service_templates');
+        expect(collectionsApiSpy).to.have.been.calledWith(
+          'service_templates',
+          { filter: ["display=true"], hide: "resources" }
+        );
+    });
+    it('should be able to handle multiple filters for service template querying', ()=>{
+      collectionsApiSpy = sinon.stub(CollectionsApi, 'query').returns(Promise.resolve(successResponse));
+      const filters =[
+        {'id':'name', 'value':'test'},
+        {'id': 'description', 'value':'test'},
+        {'id':'service_template_catalog.name', 'value':'test'}
+      ];
+      CatalogsState.setFilters(filters);
+      CatalogsState.getServiceTemplates(5,0);
+
+      expect(collectionsApiSpy).to.have.been.calledWith(
+          'service_templates',
+          {
+              expand: ['resources'],
+              attributes: ['picture', 'picture.image_href', 'service_template_catalog'],
+              limit: 5,
+              offset: 0,
+              filter: ["display=true", "name='%test%'", "description='%test%'", "service_template_catalog.name=test"],
+              sort_by: 'name',
+              sort_order: 'asc',
+              sort_options: 'ignore_case'
+          }
+        );
+    });
+  });  
 });
 
