b4a3a07fe4becde991688b0a638166dfceef7964
Allen Wight <allen.b.wight@gmail.com>
2017-06-12 09:40:06 -0400

    Normalize status and status values, fix typos

--- client/app/shared/icon-status.component.js
+++ client/app/shared/icon-status.component.js
@@ -10,23 +10,23 @@ export const IconStatusComponent = {
   },
   template: `
    <i class="pficon pficon-ok" ng-if="vm.isSuccess"
-     uib-tooltip="{{vm.stauts}}"
+     uib-tooltip="{{vm.status}}"
      tooltip-append-to-body="true"
      tooltip-popup-delay="1000"
      tooltip-placement="bottom" />
    <i class="pficon pficon-error-circle-o" ng-if="vm.isError"
-     uib-tooltip="{{vm.stauts}}"
+     uib-tooltip="{{vm.status}}"
      tooltip-append-to-body="true"
      tooltip-popup-delay="1000"
      tooltip-placement="bottom" />
    <span ng-if="vm.isInprogress" class="spinner spinner-xs spinner-inline"/>  
    <i class="fa fa-clock-o" ng-if="vm.isQueued"
-     uib-tooltip="{{vm.stauts}}"
+     uib-tooltip="{{vm.status}}"
      tooltip-append-to-body="true"
      tooltip-popup-delay="1000"
      tooltip-placement="bottom" />   
    <i class="pficon pficon-help" ng-if="vm.isUnknown"
-     uib-tooltip="{{vm.stauts}}"
+     uib-tooltip="{{vm.status}}"
      tooltip-append-to-body="true"
      tooltip-popup-delay="1000"
      tooltip-placement="bottom" />
@@ -34,14 +34,16 @@ export const IconStatusComponent = {
 };
 
 /** @ngInject */
-function ComponentController() {
+function ComponentController(lodash) {
   const vm = this;
   vm.$onChanges = function() {
+    vm.status = lodash.capitalize(vm.status);
+
     angular.extend(vm, {
-      isSuccess: vm.success ? vm.success.some((status) => status === vm.status) : false,
-      isError: vm.error ? vm.error.some((status) => status === vm.status) : false,
-      isQueued: vm.queued ? vm.queued.some((status) => status === vm.status) : false,
-      isInprogress: vm.inprogress ? vm.inprogress.some((status) => status === vm.status) : false,
+      isSuccess: vm.success ? vm.success.some((status) => status.toLowerCase() === vm.status.toLowerCase()) : false,
+      isError: vm.error ? vm.error.some((status) => status.toLowerCase() === vm.status.toLowerCase()) : false,
+      isQueued: vm.queued ? vm.queued.some((status) => status.toLowerCase() === vm.status.toLowerCase()) : false,
+      isInprogress: vm.inprogress ? vm.inprogress.some((status) => status.toLowerCase() === vm.status.toLowerCase()) : false,
     });
     vm.isUnknown = !vm.isSuccess && !vm.isError && !vm.isQueued && !vm.isInprogress;
   };
