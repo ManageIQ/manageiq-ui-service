347a1ea4f405f8bd28d9c43e4e3bed2d2d107a14
Allen Wight <allen.b.wight@gmail.com>
2017-07-31 14:33:09 -0400

    Adds additional tests to reconfigure-state [Finishes #149680043]

--- client/app/states/services/reconfigure/reconfigure.state.spec.js
+++ client/app/states/services/reconfigure/reconfigure.state.spec.js
@@ -1,23 +1,18 @@
-describe('services.reconfigure', function() {
+describe('State: services.reconfigure', function() {
   beforeEach(function() {
     module('app.states');
   });
 
   describe('controller', function() {
-    var collectionsApiSpy;
-    var controller;
-    var notificationsErrorSpy;
-    var notificationsSuccessSpy;
-    var autoRefreshSpy;
-    var refreshSingleFieldSpy;
-    var dialogFields = [{
+    let collectionsApiSpy, ctrl, notificationsErrorSpy, notificationsSuccessSpy, autoRefreshSpy, refreshSingleFieldSpy;
+    let dialogFields = [{
       name: 'dialogField1',
       default_value: '1'
     }, {
       name: 'dialogField2',
       default_value: '2'
     }];
-    var dialog = {
+    let dialog = {
       dialog_tabs: [{
         dialog_groups: [{
           dialog_fields: dialogFields
@@ -26,7 +21,7 @@ describe('services.reconfigure', function() {
     };
 
     beforeEach(function() {
-      bard.inject('$controller', '$log', '$state', '$stateParams', '$rootScope', 'CollectionsApi', 'Notifications', 'DialogFieldRefresh', 'AutoRefresh');
+      bard.inject('$controller', '$state', '$stateParams', 'CollectionsApi', 'Notifications', 'DialogFieldRefresh', 'AutoRefresh');
 
       autoRefreshSpy = sinon.stub(AutoRefresh, 'listenForAutoRefresh').callsFake(function() {
         return false;
@@ -34,7 +29,7 @@ describe('services.reconfigure', function() {
 
       refreshSingleFieldSpy = sinon.stub(DialogFieldRefresh, 'refreshSingleDialogField');
 
-      controller = $controller($state.get('services.reconfigure').controller, {
+      ctrl = $controller($state.get('services.reconfigure').controller, {
         $stateParams: {
           serviceId: 123
         },
@@ -44,7 +39,7 @@ describe('services.reconfigure', function() {
 
     describe('controller initialization', function() {
       it('is created successfully', function() {
-        expect(controller).to.be.defined;
+        expect(ctrl).to.be.defined;
       });
 
       it('listens for auto refresh messages', function() {
@@ -57,7 +52,7 @@ describe('services.reconfigure', function() {
     describe('controller#submitDialog', function() {
       describe('when the API call is successful', function() {
         beforeEach(function() {
-          var successResponse = {
+          const successResponse = {
             message: 'Great Success!'
           };
 
@@ -66,7 +61,8 @@ describe('services.reconfigure', function() {
         });
 
         it('POSTs to the service templates API', function() {
-          controller.submitDialog();
+          ctrl.submitDialog();
+
           expect(collectionsApiSpy).to.have.been.calledWith(
             'services',
             123,
@@ -75,34 +71,56 @@ describe('services.reconfigure', function() {
           );
         });
 
+        it('and canceled, does not POST to the service templates API', function() {
+          ctrl.cancelDialog();
+
+          expect(collectionsApiSpy).to.have.not.been.calledWith(
+            'services',
+            123,
+            {},
+            '{"action":"reconfigure","resource":{"href":"/api/services/123","dialogField1":"1","dialogField2":"2"}}'
+          );
+        });
+
         it('makes a notification success call', function(done) {
-          controller.submitDialog();
+          ctrl.submitDialog();
           done();
+
           expect(notificationsSuccessSpy).to.have.been.calledWith('Great Success!');
         });
 
         it('goes to the service details', function(done) {
-          controller.submitDialog();
+          ctrl.submitDialog();
           done();
+
           expect($state.is('services.details')).to.be.true;
         });
       });
 
       describe('when the API call fails', function() {
         beforeEach(function() {
-          var errorResponse = 'oopsies';
+          const errorResponse = 'oopsies';
 
           collectionsApiSpy = sinon.stub(CollectionsApi, 'post').returns(Promise.reject(errorResponse));
           notificationsErrorSpy = sinon.spy(Notifications, 'error');
         });
 
         it('makes a notification error call', function(done) {
-          controller.submitDialog();
+          ctrl.submitDialog();
           done();
+
           expect(notificationsErrorSpy).to.have.been.calledWith(
             'There was an error submitting this request: oopsies'
           );
         });
+
+        it('goes back to the service details', function(done) {
+          ctrl.backToService();
+          done();
+
+          expect($state.is('services.details')).to.be.true;
+        });
+
       });
     });
   });
