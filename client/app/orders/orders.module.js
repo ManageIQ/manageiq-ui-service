e78a00928086cd21f6a77e312ed9f2aa5d4290a9
Allen Wight <allen.b.wight@gmail.com>
2017-08-30 12:45:21 -0400

    [Finishes #150680543] Restore requests-list component

--- client/app/orders/orders.module.js
+++ client/app/orders/orders.module.js
@@ -1,6 +1,7 @@
 import { OrderExplorerComponent } from './order-explorer/order-explorer.component.js'
 import { OrdersStateFactory } from './orders-state.service.js'
 import { ProcessOrderModalComponent } from './process-order-modal/process-order-modal.component.js'
+import { RequestsListComponent } from './request-list/requests-list.component.js'
 import { SharedModule } from '../shared/shared.module.js'
 
 export const RequestsModule = angular
@@ -9,5 +10,6 @@ export const RequestsModule = angular
   ])
   .component('processOrderModal', ProcessOrderModalComponent)
   .component('orderExplorer', OrderExplorerComponent)
+  .component('requestsList', RequestsListComponent)
   .factory('OrdersState', OrdersStateFactory)
   .name
