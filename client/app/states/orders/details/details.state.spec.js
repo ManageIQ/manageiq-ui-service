3484f0231b697fd260914f8f91b5be40e24de0df
Allen Wight <allen.b.wight@gmail.com>
2017-08-30 13:25:43 -0400

    Adds request spec

---
+++ client/app/states/orders/details/details.state.spec.js
@@ -0,0 +1,29 @@
+describe('State: orders.details', () => {
+
+  beforeEach(function () {
+    module('app.states', 'app.orders', 'app.core')
+  })
+
+  let ctrl
+
+  beforeEach(() => {
+    bard.inject('$controller', '$state', '$stateParams')
+
+    ctrl = $controller($state.get('orders.details').controller, {
+      $stateParams: {
+        serviceOrderId: 213
+      },
+      order: {name: 'test order'}
+    })
+  })
+
+  describe('controller', () => {
+    it('is created successfully', () => {
+      expect(ctrl).to.be.defined
+    })
+
+    it('has an order title', () => {
+      expect(ctrl.title).to.be.eq('test order')
+    })
+  })
+})
