0d655ce5a065122788230a394c0f52d7d2399b2b
Allen Wight <allen.b.wight@gmail.com>
2017-08-30 11:33:27 -0400

    Removes unused request-state factory

--- client/app/requests/requests.module.js
+++ client/app/requests/requests.module.js
@@ -1,7 +1,6 @@
 import { OrderExplorerComponent } from './order-explorer/order-explorer.component.js'
 import { OrdersStateFactory } from './orders-state.service.js'
 import { ProcessOrderModalComponent } from './process-order-modal/process-order-modal.component.js'
-import { RequestsStateFactory } from './requests-state.service.js'
 import { SharedModule } from '../shared/shared.module.js'
 
 export const RequestsModule = angular
@@ -11,5 +10,4 @@ export const RequestsModule = angular
   .component('processOrderModal', ProcessOrderModalComponent)
   .component('orderExplorer', OrderExplorerComponent)
   .factory('OrdersState', OrdersStateFactory)
-  .factory('RequestsState', RequestsStateFactory)
   .name
