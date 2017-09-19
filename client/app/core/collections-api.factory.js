a33427ede5bb96d6af396a0c1d1262e1428a4f7d
Allen Wight <allen.b.wight@gmail.com>
2017-09-18 20:00:48 -0400

    CollectionsApi redone for extensability (infinite options), conciseness
    Behavior remains idential to before
    No need to specify an option to be added to factory (ie filter, attributes)
    Fixes tests that probably shouldn't have been passing

--- client/app/core/collections-api.factory.js
+++ client/app/core/collections-api.factory.js
@@ -1,136 +1,56 @@
-/* eslint camelcase: "off" */
-
 /** @ngInject */
-export function CollectionsApiFactory ($http, API_BASE) {
-  const service = {
+export function CollectionsApiFactory ($http, API_BASE, lodash) {
+  return {
     query: query,
     get: get,
     post: post,
-    options: options,
     delete: remove
   }
 
-  return service
-
   function query (collection, options) {
-    const url = API_BASE + '/api/' + collection
-
-    // $log.debug("query = " + url + buildQuery(options));
-
-    return $http.get(url + buildQuery(options), buildConfig(options))
+    return $http.get(`${API_BASE}/api/${collection}${buildQuery(options)}`, buildConfig(options))
       .then(handleSuccess)
-
-    function handleSuccess (response) {
-      return response.data
-    }
   }
 
   function get (collection, id, options) {
-    const url = API_BASE + '/api/' + collection + '/' + id
-
-    // $log.debug("get = " + url + buildQuery(options));
-
-    return $http.get(url + buildQuery(options), buildConfig(options))
+    return $http.get(`${API_BASE}/api/${collection}/${id}${buildQuery(options)}`, buildConfig(options))
       .then(handleSuccess)
-
-    function handleSuccess (response) {
-      return response.data
-    }
   }
 
   function post (collection, id, options, data) {
-    const url = API_BASE + '/api/' + collection + '/' + (id || '') + buildQuery(options)
-
-    // $log.debug("post = " + url + buildQuery(options));
-
-    return $http.post(url, data, buildConfig(options))
+    return $http.post(`${API_BASE}/api/${collection}/${id || ''}${buildQuery(options)}`, data, buildConfig(options))
       .then(handleSuccess)
-
-    function handleSuccess (response) {
-      return response.data
-    }
   }
 
-  function options (collection) {
-    const url = API_BASE + '/api/' + collection
-
-    return $http.get(url + buildQuery(options))
-      .then(handleSuccess)
-
-    function handleSuccess (response) {
-      return response.data
-    }
-  }
-
-  // delete is a reserved word in JS
   function remove (collection, id, options) {
-    const url = API_BASE + '/api/' + collection + '/' + (id || '') + buildQuery(options)
-
-    // $log.debug("post = " + url + buildQuery(options));
-
-    return $http.delete(url, buildConfig(options))
+    return $http.delete(`${API_BASE}/api/${collection}/${id || ''}${buildQuery(options)}`, buildConfig(options))
       .then(handleSuccess)
-
-    function handleSuccess (response) {
-      return response.data
-    }
   }
 
   // Private
+  function handleSuccess (response) {
+    return response.data
+  }
 
   function buildQuery (options) {
     const params = []
-    options = options || {}
 
-    if (options.expand) {
-      if (angular.isArray(options.expand)) {
-        options.expand = options.expand.join(',')
+    lodash.forEach(options, (value, key) => {
+      switch (key) {
+        case 'filter':
+          lodash.forEach(value, (filter) => {
+            params.push('filter[]=' + encodeURIComponent(filter))
+          })
+          break
+        case 'auto_refresh':
+          break
+        default:
+          params.push(`${key}=${encodeURIComponent(joinArray(value))}`)
       }
-      params.push('expand=' + encodeURIComponent(options.expand))
-    }
+    })
 
-    if (options.attributes) {
-      if (angular.isArray(options.attributes)) {
-        options.attributes = options.attributes.join(',')
-      }
-      params.push('attributes=' + encodeURIComponent(options.attributes))
-    }
-
-    if (options.format_attributes) {
-      if (angular.isArray(options.format_attributes)) {
-        options.format_attributes = options.format_attributes.join(',')
-      }
-      params.push('format_attributes=' + encodeURIComponent(options.format_attributes))
-    }
-
-    if (options.filter) {
-      angular.forEach(options.filter, function (filter) {
-        params.push('filter[]=' + encodeURIComponent(filter))
-      })
-    }
-
-    if (options.limit) {
-      params.push('limit=' + encodeURIComponent(options.limit))
-    }
-
-    if (options.offset) {
-      params.push('offset=' + encodeURIComponent(options.offset))
-    }
-
-    if (options.hide) {
-      params.push('hide=' + encodeURIComponent(options.hide))
-    }
-
-    if (options.sort_by) {
-      params.push('sort_by=' + encodeURIComponent(options.sort_by))
-    }
-
-    if (options.sort_order) {
-      params.push('sort_order=' + encodeURIComponent(options.sort_order))
-    }
-
-    if (options.sort_options) {
-      params.push('sort_options=' + encodeURIComponent(options.sort_options))
+    function joinArray (value) {
+      return Array.isArray(value) ? value.join(',') : value
     }
 
     return params.length ? '?' + params.join('&') : ''
--- client/app/core/collections-api.factory.spec.js
+++ client/app/core/collections-api.factory.spec.js
@@ -1,52 +1,57 @@
-describe('Collections API', function() {
-    beforeEach(function() {
-        module('app.core');
-        bard.inject('CollectionsApi', '$http');
-    });
-    const successResponse = {
-     'data':{
-            'status': 'success' 
-            } 
-    };
+describe('Collections API', function () {
+  beforeEach(function () {
+    module('app.core')
+    bard.inject('CollectionsApi', '$http')
+  })
+  const successResponse = {
+    'data': {
+      'status': 'success'
+    }
+  }
 
-    it('should allow a query to be run', () => {
-      const getSpy = sinon.stub($http, 'get').returns(Promise.resolve(successResponse));
-        return CollectionsApi.query('services', '1', {}).then((data) => {
-        expect(data).to.eql({'status':'success'});
-        });
-    });
-    it('should allow a get request to be run', () => {
-      const getSpy = sinon.stub($http, 'get').returns(Promise.resolve(successResponse));
-        return CollectionsApi.get('services', {}).then((data) => {
-        expect(data).to.eql({'status':'success'});
-        });
-    });
-    it('should allow a post request to be run', () => {
-      const postSpy = sinon.stub($http, 'post').returns(Promise.resolve(successResponse));
-        return CollectionsApi.post('services', {}).then((data) => {
-        expect(data).to.eql({'status':'success'});
-        });
-    });
-    it('should allow a options request to be run', () => {
-      const getSpy = sinon.stub($http, 'get').returns(Promise.resolve(successResponse));
-        return CollectionsApi.options('services', {}).then((data) => {
-        expect(data).to.eql({'status':'success'});
-        });
-    });
-    it('should allow a delete request to be run', () => {
-      const deleteSpy = sinon.stub($http, 'delete').returns(Promise.resolve(successResponse));
-        return CollectionsApi.delete('services','1',{}).then((data) => {
-        expect(data).to.eql({'status':'success'});
-        });
-    });
-    it('should build up a get request', () => {
-        const getSpy = sinon.stub($http, 'get').returns(Promise.resolve(successResponse)); 
-        const options = {
-            limit: 10,
-            offset: 10,
-            format_attributes: ['option1','option2']
-        }
-         CollectionsApi.get('services', '1', options);
-         expect(getSpy).to.have.been.calledWith('http://localhost:9876/api/services/1?format_attributes=option1%2Coption2&limit=10&offset=10',{});
-    });
-});
+  it('should allow a query to be run', () => {
+    const getSpy = sinon.stub($http, 'get').returns(Promise.resolve(successResponse))
+    return CollectionsApi.query('services', '1', {}).then((data) => {
+      expect(data).to.eql({'status': 'success'})
+    })
+  })
+  it('should allow a get request to be run', () => {
+    const getSpy = sinon.stub($http, 'get').returns(Promise.resolve(successResponse))
+    return CollectionsApi.get('services', {}).then((data) => {
+      expect(data).to.eql({'status': 'success'})
+    })
+  })
+  it('should allow a post request to be run', () => {
+    const postSpy = sinon.stub($http, 'post').returns(Promise.resolve(successResponse))
+    return CollectionsApi.post('services', {}).then((data) => {
+      expect(data).to.eql({'status': 'success'})
+    })
+  })
+  it('should allow a delete request to be run', () => {
+    const deleteSpy = sinon.stub($http, 'delete').returns(Promise.resolve(successResponse))
+    return CollectionsApi.delete('services', '1', {}).then((data) => {
+      expect(data).to.eql({'status': 'success'})
+    })
+  })
+  it('should build up a delete request with id', () => {
+    const getSpy = sinon.stub($http, 'delete').returns(Promise.resolve(successResponse))
+    const options = {auto_refresh: true}
+    CollectionsApi.delete('services', '101', options)
+    expect(getSpy.getCall(0).args[0]).to.eq('http://localhost:9876/api/services/101')
+  })
+  it('should build up a delete request without id', () => {
+    const getSpy = sinon.stub($http, 'delete').returns(Promise.resolve(successResponse))
+    CollectionsApi.delete('services')
+    expect(getSpy.getCall(0).args[0]).to.eq('http://localhost:9876/api/services/')
+  })
+  it('should build up a get request', () => {
+    const getSpy = sinon.stub($http, 'get').returns(Promise.resolve(successResponse))
+    const options = {
+      limit: 10,
+      offset: 10,
+      format_attributes: ['option1', 'option2']
+    }
+    CollectionsApi.get('services', '1', options)
+    expect(getSpy).to.have.been.calledWith('http://localhost:9876/api/services/1?limit=10&offset=10&format_attributes=option1%2Coption2')
+  })
+})
