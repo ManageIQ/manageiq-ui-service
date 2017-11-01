2b598628e01c194e52d08a87d71f4359429edf39
Allen Wight <awight@redhat.com>
2017-11-01 10:34:13 -0400

    Reword supports_launch_cockpit for correct supports_cockpit

--- client/app/services/services-state.service.js
+++ client/app/services/services-state.service.js
@@ -30,7 +30,7 @@ export function ServicesStateFactory(ListConfiguration, CollectionsApi, RBAC) {
         'retirement_state', 'retirement_warn', 'retires_on', 'actions', 'custom_actions', 'provision_dialog', 'service_resources',
         'chargeback_report', 'service_template', 'parent_service', 'power_state', 'power_status', 'options', 'vms.ipaddresses',
         'vms.snapshots', 'vms.v_total_snapshots', 'vms.v_snapshot_newest_name', 'vms.v_snapshot_newest_timestamp', 'vms.v_snapshot_newest_total_size',
-        'vms.supports_console?', 'vms.supports_launch_cockpit?',
+        'vms.supports_console?', 'vms.supports_cockpit?',
       ],
       expand: ['vms', 'orchestration_stacks'],
       auto_refresh: refresh,
