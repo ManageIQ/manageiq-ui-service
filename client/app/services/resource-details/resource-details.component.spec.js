0b8e5730075e7feead9d824be159191f2124679a
Allen Wight <allen.b.wight@gmail.com>
2017-12-08 07:10:47 -0500

    Merge pull request #1254 from chalettu/custom-button-grouping
    Added custom button menu to Resource Details page
    (cherry picked from commit 672d1281341c8e7f818d1779682679e9727abfa4)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1524718

--- client/app/services/resource-details/resource-details.component.spec.js
+++ client/app/services/resource-details/resource-details.component.spec.js
@@ -54,9 +54,9 @@ describe('Component: Resource Details', () => {
     it('should be able to check for custom buttons', (done) => {
       vmSpy = sinon.stub(VmsService, 'getVm').returns(Promise.resolve(vmData))
       ctrl.vmDetails.custom_actions = vmData.custom_actions
-      const hasCustomButtons = ctrl.hasCustomButtons()
+      const customButtonCount = ctrl.customButtonCount()
       done()
-      expect(hasCustomButtons).to.be.true
+      expect(customButtonCount).to.eq(3)
     })
     it('should have list actions', (done) => {
       vmSpy = sinon.stub(VmsService, 'getVm').returns(Promise.resolve(vmData))
