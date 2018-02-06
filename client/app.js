d5993c9cb8504bbd73344585603634296a573547
Chris Hale <chris.hale@me.com>
2018-02-06 10:30:20 -0700

    Added global for patternflyVersion
    This was added in support of https://github.com/ManageIQ/ui-components/pull/249 which deals with different elements based on patternfly version in UI-components repo.

--- client/app.js
+++ client/app.js
@@ -12,7 +12,7 @@ window.moment = require('moment')
 window.sprintf = require('sprintf-js').sprintf
 window.c3 = require('c3/c3.js')
 window.d3 = require('d3/d3.js')
-
+window.patternflyVersion = 4
 // Vendor libraries, order matters
 require('jquery-ui-bundle')
 require('moment-timezone')
