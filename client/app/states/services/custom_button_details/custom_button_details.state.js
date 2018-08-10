0dede2d65b0c0e725c2745a87ed9ad080b3afb8e
Chris Hale <chris.hale@me.com>
2018-08-10 13:59:20 -0600

    Updated URL for custom buttons dialogs

--- client/app/states/services/custom_button_details/custom_button_details.state.js
+++ client/app/states/services/custom_button_details/custom_button_details.state.js
@@ -48,9 +48,16 @@ function StateController ($state, $stateParams, CollectionsApi, EventNotificatio
   vm.loading = true
 
   function init () {
-    const options = {expand: 'resources', attributes: 'content'}
+    const options = {
+      expand: 'resources',
+      attributes: 'content',
+      resource_action_id: vm.resourceAction.id,
+      target_id: vm.serviceId,
+      target_type: 'service'
+    }
+
     const dialogId = vm.resourceAction.dialog_id
-    const resolveDialogs = CollectionsApi.query(`services/${vm.serviceId}/service_dialogs/${dialogId}`, options)
+    const resolveDialogs = CollectionsApi.query(`service_dialogs/${dialogId}`, options)
     const resolveService = CollectionsApi.get('services', $stateParams.serviceId, {attributes: ['picture', 'picture.image_href']})
 
     Promise.all([resolveDialogs, resolveService]).then((data) => {
