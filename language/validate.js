ede557051037c56fd822ab72db4f7504cd1e2702
Martin Hradil <mhradil@redhat.com>
2019-09-17 15:06:31 +0000

    language/validate - angular template strings are OK

--- language/validate.js
+++ language/validate.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const potFile = path.join(__dirname, '../client/gettext/po/manageiq-ui-service.pot')
 const contents = fs.readFileSync(potFile, 'utf8')
-const re = /<(.|\n)*>|{{(.|\n)*}}/gim // checks for html and also for javascript
+const re = /<(.|\n)*>/gim // checks for html
 const matches = contents.match(re)
 
 if (matches != null) {
