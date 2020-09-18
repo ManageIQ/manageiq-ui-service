1f5abddf58d6ddba27d66f5933bb21b86fd00f62
Martin Hradil <mhradil@redhat.com>
2020-09-18 21:35:43 +0000

    Merge pull request #1673 from Fryguy/include_modules_manifest
    Include modules manifest
    
    (cherry picked from commit 4fb87bcbabecb7cb74a685c512d950c95b88aad8)

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -9,6 +9,52 @@ const sortBy = require('lodash/sortBy');
 
 const config = require('./webpack.dev.js')
 
+const uniqueWebpackModules = (data) => {
+  // recursively flatten nested modules
+  const transformModule  = m  => m.modules ? transformModules(m.modules) : m.name.split("!").pop();
+  const transformModules = ms => ms.flatMap(m => transformModule(m));
+
+  var modules = transformModules(data.modules);
+  modules = [...new Set(modules)]; // uniq
+
+  return modules;
+}
+
+const packagesFromModules = (modules) => {
+  // find the package.json file for each module
+  modules = modules.filter(m => m.includes("node_modules"));
+  var packagePaths = modules.map(m => {
+    var match = m.match(/(.*node_modules\/)([^/]+)(\/[^/]+)?/);
+    var path = [
+      `${match[1]}${match[2]}/package.json`,
+      `${match[1]}${match[2]}${match[3]}/package.json`,
+    ].find(p => fs.existsSync(p));
+
+    if (path == null) {
+      console.warn(`[webpack-manifest] WARN: Unable to find a package.json for ${m}`);
+    }
+
+    return path;
+  })
+  packagePaths = packagePaths.filter(p => p != null);
+  packagePaths = [...new Set(packagePaths)]; // uniq
+
+  // extract relevant package data from the package.json
+  var packages = packagePaths.map(p => {
+    var content = fs.readFileSync(p);
+    var pkg = JSON.parse(content);
+    return {
+      name: pkg.name,
+      license: pkg.license,
+      version: pkg.version,
+      location: p
+    }
+  });
+  packages = sortBy(packages, ['name', 'version']);
+
+  return packages;
+}
+
 // Source maps suitable for production use
 config.devtool = 'cheap-module-source-map'
 
@@ -31,50 +77,25 @@ config.plugins.push(
 
   new OptimizeCssAssetsWebpackPlugin(),
 
-  // Write out dependencies list for audit purposes
+  // Write out dependent modules list for audit purposes
   new StatsWriterPlugin({
-    filename: "webpack-modules-manifest.json",
+    filename: "webpack_modules_manifest.json",
     fields: ["modules"],
 
     transform(data) {
-      // recursively flatten nested modules
-      const transformModule  = m  => m.modules ? transformModules(m.modules) : m.name.split("!").pop();
-      const transformModules = ms => ms.flatMap(m => transformModule(m));
+      var modules = uniqueWebpackModules(data);
+      return JSON.stringify(modules, null, 2);
+    }
+  }),
 
-      var modules = transformModules(data.modules)
-      modules = [...new Set(modules)]; // uniq
+  // Write out dependent packages list for audit purposes
+  new StatsWriterPlugin({
+    filename: "webpack_packages_manifest.json",
+    fields: ["modules"],
 
-      // find the package.json file for each module
-      modules = modules.filter(m => m.includes("node_modules"));
-      var packagePaths = modules.map(m => {
-        var match = m.match(/(.*node_modules\/)([^/]+)(\/[^/]+)?/);
-        var path = [
-          `${match[1]}${match[2]}/package.json`,
-          `${match[1]}${match[2]}${match[3]}/package.json`,
-        ].find(p => fs.existsSync(p));
-
-        if (path == null) {
-          console.warn(`[webpack-manifest] WARN: Unable to find a package.json for ${m}`);
-        }
-
-        return path;
-      })
-      packagePaths = packagePaths.filter(p => p != null);
-      packagePaths = [...new Set(packagePaths)]; // uniq
-
-      // extract relevant package data from the package.json
-      var packages = packagePaths.map(p => {
-        var content = fs.readFileSync(p);
-        var pkg = JSON.parse(content);
-        return {
-          name: pkg.name,
-          license: pkg.license,
-          version: pkg.version,
-          location: p
-        }
-      });
-      packages = sortBy(packages, ['name', 'version']);
-
+    transform(data) {
+      var modules = uniqueWebpackModules(data);
+      var packages = packagesFromModules(modules);
       return JSON.stringify(packages, null, 2);
     }
   })
