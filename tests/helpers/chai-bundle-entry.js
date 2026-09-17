a6ae2f662b951b1a878b39593bf12a1679a29396
Gilbert Cherrie <g.cherrie@outlook.com>
2026-09-17 13:10:48 -0400

    Remove unneeded lines

--- tests/helpers/chai-bundle-entry.js
+++ tests/helpers/chai-bundle-entry.js
@@ -4,7 +4,4 @@ import sinonChai from 'sinon-chai'
 chai.use(sinonChai)
 
 // Expose as individual globals that tests and karma-sinon expect
-window.chai   = chai
 window.expect = chai.expect
-window.assert = chai.assert
-window.should = chai.should()
